8169079a69075f794d401f3ee293c9563331a606
harshana <harshana@a5903396-d722-0410-b921-86c7d4935375>
2012-03-16 20:44:39 +0000

    Fixed TOOLS-894
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@122861 a5903396-d722-0410-b921-86c7d4935375

---
+++ ide/eclipse/core/org.wso2.developerstudio.eclipse.platform.core/src/org/wso2/developerstudio/eclipse/platform/core/project/refactor/ArtifactProjectRenameRefactorParticipant.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2012, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.developerstudio.eclipse.platform.core.project.refactor;
+
+import org.apache.maven.model.Dependency;
+import org.apache.maven.project.MavenProject;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.ltk.core.refactoring.Change;
+import org.eclipse.ltk.core.refactoring.CompositeChange;
+import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext;
+import org.eclipse.ltk.core.refactoring.participants.RenameArguments;
+import org.eclipse.ltk.core.refactoring.participants.RenameParticipant;
+import org.wso2.developerstudio.eclipse.maven.util.MavenUtils;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class ArtifactProjectRenameRefactorParticipant extends RenameParticipant {
+	private IProject originalProject;
+	private String latestName;
+
+	@Override
+    public RefactoringStatus checkConditions(IProgressMonitor arg0, CheckConditionsContext arg1)
+                                                                                                throws OperationCanceledException {
+	    return RefactoringStatus.createWarningStatus("You are about to rename your project and configuration from "+originalProject.getName()+" to "+latestName);
+    }
+	
+	@Override
+    public Change createPreChange(IProgressMonitor arg0) throws CoreException,
+                                                     OperationCanceledException {
+		
+		IFile pomFile = originalProject.getFile("pom.xml");
+		
+		CompositeChange artifactChange=new CompositeChange("Maven Configuration Refactor");
+		
+		MavenConfigurationFileChange endpointArtifactFileChange =new MavenConfigurationFileChange("HW", pomFile, originalProject.getName(), originalProject, latestName); 
+		artifactChange.add(endpointArtifactFileChange);
+		
+		
+		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+		
+		for (int i = 0; i < projects.length; i++) {
+	        if(projects[i].isOpen() && projects[i].hasNature("org.wso2.developerstudio.eclipse.distribution.project.nature")){
+	        	try {
+	                MavenProject mavenProject = MavenUtils.getMavenProject(projects[i].getFile("pom.xml").getLocation().toFile());
+	                List dependencies = mavenProject.getDependencies();
+	                for (Iterator iterator = dependencies.iterator(); iterator.hasNext();) {
+	                    Dependency dependency = (Dependency) iterator.next();
+	                    if(originalProject.getName().equalsIgnoreCase(dependency.getArtifactId())){
+	                    	artifactChange.add(new MavenConfigurationFileChange(projects[i].getName(), projects[i].getFile("pom.xml"), originalProject.getName(), projects[i], latestName));
+	                    }
+                    }
+                } catch (Exception e) {
+	                // TODO Auto-generated catch block
+	                e.printStackTrace();
+                }
+	        }
+        }
+		
+		
+	    return artifactChange;
+    }
+
+	@Override
+    public String getName() {
+	    // TODO Auto-generated method stub
+	    return "Refactor Maven Configurations";
+    }
+
+	@Override
+    protected boolean initialize(Object arg0) {
+		if(arg0 instanceof IProject){
+			originalProject = (IProject) arg0;
+			RenameArguments arguments = getArguments();
+			latestName = arguments.getNewName();
+			return true;
+		}
+	    return false;
+    }
+
+	@Override
+    public Change createChange(IProgressMonitor arg0) throws CoreException,
+                                                     OperationCanceledException {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+}
