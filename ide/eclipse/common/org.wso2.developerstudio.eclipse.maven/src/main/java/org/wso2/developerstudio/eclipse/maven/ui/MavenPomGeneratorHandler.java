3033a5da2e2450d980962aa0f565743f162be10a
harshana <harshana@a5903396-d722-0410-b921-86c7d4935375>
2012-04-16 12:22:03 +0000

    Implementing feature TOOLS-890
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@125022 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/common/org.wso2.developerstudio.eclipse.maven/src/main/java/org/wso2/developerstudio/eclipse/maven/ui/MavenPomGeneratorHandler.java
+++ ide/eclipse/common/org.wso2.developerstudio.eclipse.maven/src/main/java/org/wso2/developerstudio/eclipse/maven/ui/MavenPomGeneratorHandler.java
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- *
+ * Copyright (c) 2012, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,30 +19,45 @@ package org.wso2.developerstudio.eclipse.maven.ui;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
 
 /**
  * This is the handler class for generating maven pom for any given project option.
- * This is used to provide maven multi module support for Developer Studio. 
+ * This is used to provide maven multi module support for Developer Studio.
  * 
  */
-public class MavenPomGeneratorHandler extends AbstractHandler{
+public class MavenPomGeneratorHandler extends AbstractHandler {
 
 	public Object execute(ExecutionEvent arg0) throws ExecutionException {
 
-		 System.out.println("command Executed");
-		 
-		 
-		// TODO: implement the Pom Generation Logic 
-//		IStructuredSelection selection =
-//		                                 (IStructuredSelection) HandlerUtil.getCurrentSelectionChecked(arg0);
-//		IServerModule object = (IServerModule) selection.getFirstElement();
-//
-//		// There is always only one module in the IServerModule for our case.
-//		String projectName = object.getModule()[0].getName();
-//		IServer server = object.getServer();
-//
-//		CarbonServerManager.getInstance().redeployServiceModule(server.getId(), WEB_TEMP_PATH,
-//				projectName);
+		System.out.println("command Executed");
+
+		IStructuredSelection selection =
+		                                 (IStructuredSelection) HandlerUtil.getCurrentSelectionChecked(arg0);
+		IProject selectedUIElement = (IProject) selection.getFirstElement();
+
+		IFile pomFile = null;
+		boolean openQuestion = false;
+		
+		if (selectedUIElement.isOpen()) {
+			pomFile = selectedUIElement.getFile("pom.xml");
+			if (pomFile.exists()) {
+				openQuestion =
+				               MessageDialog.openQuestion(Display.getDefault().getActiveShell(),
+				                                          "Generate POM for the Project",
+				                                          "Do you want to convert this project to a maven multi module project?");
+			}
+		}
+		
+		if(!pomFile.exists() || (pomFile.exists() && openQuestion)){
+//			 TODO: implement the Pom Generation Logic
+		}
+
 		return null;
 	}
 
