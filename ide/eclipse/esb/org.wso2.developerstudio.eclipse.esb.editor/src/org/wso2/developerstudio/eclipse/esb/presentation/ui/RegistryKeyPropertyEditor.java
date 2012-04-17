2ce7f8f5e620c3a239e2e2d8f241f7087293d868
melan <melan@a5903396-d722-0410-b921-86c7d4935375>
2012-04-17 06:25:24 +0000

    Fixing resource template dialog
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@125058 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.esb.editor/src/org/wso2/developerstudio/eclipse/esb/presentation/ui/RegistryKeyPropertyEditor.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.esb.editor/src/org/wso2/developerstudio/eclipse/esb/presentation/ui/RegistryKeyPropertyEditor.java
@@ -81,7 +81,7 @@ public class RegistryKeyPropertyEditor extends CustomDialogCellEditor {
 		RegistryKeyPropertyEditorDialog dialog = new RegistryKeyPropertyEditorDialog(cellEditorWindow.getShell(),
 				getStyle(), registryKeyProperty, findLocalNamedEntities(propertyContainer));
 		dialog.create();
-		dialog.getShell().setSize(520,180);
+		dialog.getShell().setSize(520,220);
 		dialog.getShell().setText("Resource Key Editor");
 		dialog.open();
 		
