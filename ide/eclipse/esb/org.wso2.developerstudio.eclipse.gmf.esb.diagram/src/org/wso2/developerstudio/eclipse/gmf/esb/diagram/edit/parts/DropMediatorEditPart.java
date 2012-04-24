7e33620f187b609bcee16ee230107e04d6617840
viraj <viraj@a5903396-d722-0410-b921-86c7d4935375>
2012-04-24 10:41:59 +0000

    Change the color of Drop Mediator.
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@125788 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/edit/parts/DropMediatorEditPart.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/edit/parts/DropMediatorEditPart.java
@@ -602,8 +602,8 @@ public class DropMediatorEditPart extends AbstractMediator {
 	}
 
 	/**
-	 * @generated
+	 * @generated NOT
 	 */
-	static final Color THIS_BACK = new Color(null, 230, 0, 0);
+	static final Color THIS_BACK = new Color(null, 230, 230, 230);
 
 }
