0894fd779afde5fb18e4a59a8d88168068946c8a
viraj <viraj@a5903396-d722-0410-b921-86c7d4935375>
2012-06-01 10:16:32 +0000

    Removing ESB edior
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@129047 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.sequence.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/sequence/diagram/edit/helpers/CalloutMediatorOutputConnectorEditHelper.java
+++
@@ -1,7 +0,0 @@
-package org.wso2.developerstudio.eclipse.gmf.esb.sequence.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class CalloutMediatorOutputConnectorEditHelper extends EsbBaseEditHelper {
-}
