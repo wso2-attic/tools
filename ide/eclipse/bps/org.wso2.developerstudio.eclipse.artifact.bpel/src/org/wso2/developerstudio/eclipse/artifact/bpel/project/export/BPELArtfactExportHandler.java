770ddbdd1db3167b23b9e58d611df6f3a83a861e
melan <melan@a5903396-d722-0410-b921-86c7d4935375>
2012-06-05 13:15:26 +0000

    Fixing TOOLS-1114
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@129268 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/bps/org.wso2.developerstudio.eclipse.artifact.bpel/src/org/wso2/developerstudio/eclipse/artifact/bpel/project/export/BPELArtfactExportHandler.java
+++ ide/eclipse/bps/org.wso2.developerstudio.eclipse.artifact.bpel/src/org/wso2/developerstudio/eclipse/artifact/bpel/project/export/BPELArtfactExportHandler.java
@@ -17,89 +17,74 @@
 package org.wso2.developerstudio.eclipse.artifact.bpel.project.export;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileFilter;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.osgi.storagemanager.StorageManager;
 import org.wso2.developerstudio.eclipse.platform.core.project.export.ProjectArtifactHandler;
 import org.wso2.developerstudio.eclipse.utils.archive.ArchiveManipulator;
 import org.wso2.developerstudio.eclipse.utils.file.FileUtils;
+import org.wso2.developerstudio.eclipse.utils.file.TempFileUtils;
 
 public class BPELArtfactExportHandler extends ProjectArtifactHandler {
+	private static List<String> excludeList=new ArrayList<String>();
 
+	static {
+		excludeList.add("pom.xml");
+		excludeList.add("build.xml");
+		excludeList.add(".project");
+		excludeList.add(".classpath");
+		excludeList.add(".svn");
+		excludeList.add("target");
+		excludeList.add(".settings");
+	}
 
-	public List<IResource> exportArtifact(IProject project) {
+	public List<IResource> exportArtifact(IProject project) throws Exception {
 		List<IResource> exportResources = new ArrayList<IResource>();
-		if(!project.isOpen()){
-			return exportResources;
-		}
-		
-		try {
-			ArchiveManipulator archiveManipulator = new ArchiveManipulator();
-			
-			//First compile the code
-	        NullProgressMonitor nullProgressMonitor = new NullProgressMonitor();
-	        //Let's create a temp project 
-	        IProject tempProject = ResourcesPlugin.getWorkspace().getRoot().getProject(".temp"+System.currentTimeMillis());
-	        tempProject.create(nullProgressMonitor);
-	        tempProject.open(nullProgressMonitor);
-	        tempProject.setHidden(true);
-	        
-	        org.eclipse.osgi.storagemanager.StorageManager manager=new StorageManager(tempProject.getLocation().toFile(), "false");
-	        
-	        File bpelResources = manager.createTempFile("bpel_resources");
-	        bpelResources.delete();
-	        bpelResources.mkdir();
 
-	        FileUtils.copyDirectoryContents(project.getLocation().toFile(), bpelResources); 
-	        
-	        //cleaning unnecessary files before archiving
-			String[] excludeFiles = {"pom.xml", ".project"};
-			String[] excludeDir = {"target", ".settings" };
-			IFolder bpelResDir = tempProject.getFolder(bpelResources.getName());
-			
-			for (String excludeFile : excludeFiles) {
-				IFile excludeRes = bpelResDir.getFile(excludeFile);
-				if (excludeRes.getLocation().toFile().exists()) {
-				
-					excludeRes.getLocation().toFile().delete();
-				}
-			}
-			for (String excludeFile : excludeDir) {
-				IFolder excludeRes = bpelResDir.getFolder(excludeFile);
-				if (excludeRes.getLocation().toFile().exists()) {
-					FileUtils.deleteDir(excludeRes.getLocation().toFile());
-				}
-			}
+		ArchiveManipulator archiveManipulator = new ArchiveManipulator();
 
-	        File tmpArchive = new File(tempProject.getLocation().toFile(),project.getName().concat(".zip"));
-	        archiveManipulator.archiveDir(tmpArchive.toString(), bpelResources.toString());
-	        IFolder binaries = project.getFolder("target");
-	        if(!binaries.exists()) {
-	        	binaries.create(true, true, nullProgressMonitor);
-	        	binaries.setHidden(true);
-	        }
-	        IFile bpelArchive = project.getFile("target/"+ project.getName().concat(".zip"));
-	        FileUtils.copy(tmpArchive, bpelArchive.getLocation().toFile());
-	        exportResources.add((IResource)bpelArchive);
-	        
-	        //cleaning temp project
-	        tempProject.delete(true, nullProgressMonitor);
-	        
-        } catch (CoreException e) {
-	        e.printStackTrace();
-        } catch (IOException e) {
-	        e.printStackTrace();
-        }
-		
+		clearTarget(project);
+		File tempProject = createTempProject();
+		File bpelResources = createTempDir(tempProject, "bpel_resources");
+		org.apache.commons.io.FileUtils.copyDirectory(project.getLocation().toFile(),
+				bpelResources, new FileFilter() {
+			
+					@Override
+					public boolean accept(File pathname) {
+						if(pathname.isDirectory()){
+							if(excludeList.contains(pathname.getName())) {
+								return false;
+							}
+							File[] listFiles = pathname.listFiles();
+							if(listFiles.length==1){
+								if(excludeList.contains(listFiles[0].getName())){
+									return false;
+								}
+							} else if (listFiles.length==0){
+								return false;
+							}
+						} else {
+							if(excludeList.contains(pathname.getName())) {
+								return false;
+							}
+						}
+						return true;
+					}
+				});
+
+		File tmpArchive = new File(tempProject, project.getName().concat(".zip"));
+		archiveManipulator.archiveDir(tmpArchive.toString(),
+				bpelResources.toString());
+		IFile bpelArchive = getTargetArchive(project, "zip");
+		FileUtils.copy(tmpArchive, bpelArchive.getLocation().toFile());
+		exportResources.add((IResource) bpelArchive);
+
+		TempFileUtils.cleanUp();
+
 		return exportResources;
 	}
 
