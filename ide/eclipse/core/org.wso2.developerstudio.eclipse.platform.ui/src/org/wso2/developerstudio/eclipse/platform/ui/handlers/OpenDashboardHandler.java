c56ccfcecf22d1d1e2a20a5d3880cea73fef44ec
melan <melan@a5903396-d722-0410-b921-86c7d4935375>
2012-06-19 04:45:22 +0000

    Fixing TOOLS-1127
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@130431 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/core/org.wso2.developerstudio.eclipse.platform.ui/src/org/wso2/developerstudio/eclipse/platform/ui/handlers/OpenDashboardHandler.java
+++ ide/eclipse/core/org.wso2.developerstudio.eclipse.platform.ui/src/org/wso2/developerstudio/eclipse/platform/ui/handlers/OpenDashboardHandler.java
@@ -15,15 +15,22 @@
 
 package org.wso2.developerstudio.eclipse.platform.ui.handlers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IPersistableElement;
+import org.eclipse.ui.IViewReference;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
 import org.wso2.developerstudio.eclipse.logging.core.IDeveloperStudioLog;
 import org.wso2.developerstudio.eclipse.logging.core.Logger;
 import org.wso2.developerstudio.eclipse.platform.core.Activator;
@@ -31,17 +38,60 @@ import org.wso2.developerstudio.eclipse.platform.core.Activator;
 public class OpenDashboardHandler  extends AbstractHandler {
 	
 	private static IDeveloperStudioLog log=Logger.getLog(Activator.PLUGIN_ID);
+	static final String INTRO_VIEW_ID = "org.eclipse.ui.internal.introview";
+	static final String DASHBOARD_VIEW_ID = "org.wso2.developerstudio.eclipse.dashboard";
+	static final String J2EE_PERSPECTIVE_ID = "org.eclipse.jst.j2ee.J2EEPerspective";
+	
 
     public Object execute(ExecutionEvent event) throws ExecutionException {
 		  IWorkbenchWindow window=PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 	        IWorkbenchPage page = window.getActivePage();
 	        try {
-	        	PlatformUI.getWorkbench().showPerspective("org.eclipse.jst.j2ee.J2EEPerspective", window);
-				page.openEditor(new NullEditorInput(), "org.wso2.developerstudio.eclipse.dashboard");
+	        	hideIntroView();
+	        	hideDashboards();
+	        	PlatformUI.getWorkbench().showPerspective(J2EE_PERSPECTIVE_ID, window);
+				page.openEditor(new NullEditorInput(), DASHBOARD_VIEW_ID);
 			} catch (Exception e) {
 				log.error("Cannot open dashboard", e);
 			}
 	    return true;
+    }
+    
+    /**
+     * hide eclipse welcome page
+     */
+    private void hideIntroView(){
+    	try {
+    		 IWorkbenchWindow window=PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+    		 IWorkbenchPage page = window.getActivePage();
+    		 IViewReference ref = page.findViewReference(INTRO_VIEW_ID);
+    		 page.hideView(ref);
+		} catch (Exception e) {
+			/* safe to ignore */
+		}
+    }
+    
+    /**
+     * hide open dashboards
+     */
+    private void hideDashboards(){
+    	try {
+    		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+			IWorkbenchPage page = window.getActivePage();
+			List<IEditorReference> openEditors = new ArrayList<IEditorReference>();
+			IEditorReference[] editorReferences = PlatformUI.getWorkbench()
+					.getActiveWorkbenchWindow().getActivePage().getEditorReferences();
+			for (IEditorReference iEditorReference : editorReferences) {
+				if (DASHBOARD_VIEW_ID.equals(iEditorReference.getId())) {
+					openEditors.add(iEditorReference);
+				}
+			}
+			if (openEditors.size() > 0) {
+				page.closeEditors(openEditors.toArray(new IEditorReference[] {}), false);
+			}
+		} catch (Exception e) {
+			/* safe to ignore */
+		}
     }
 	
 	class NullEditorInput implements IEditorInput {
