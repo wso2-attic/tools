ab4e9804d254a5acc2098869169741ecb4d4940d
melan <melan@a5903396-d722-0410-b921-86c7d4935375>
2012-07-25 11:43:08 +0000

    Adding JAX-RS nature
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@134859 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/core/org.wso2.developerstudio.eclipse.platform.ui/src/org/wso2/developerstudio/eclipse/platform/ui/mvn/wizard/MvnMultiModuleProjectList.java
+++ ide/eclipse/core/org.wso2.developerstudio.eclipse.platform.ui/src/org/wso2/developerstudio/eclipse/platform/ui/mvn/wizard/MvnMultiModuleProjectList.java
@@ -1,13 +1,24 @@
+/*
+ * Copyright (c) 2012, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.developerstudio.eclipse.platform.ui.mvn.wizard;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.stream.FactoryConfigurationError;
-
-import org.apache.maven.model.Dependency;
-import org.apache.maven.project.MavenProject;
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
@@ -40,6 +51,7 @@ private static IDeveloperStudioLog log=Logger.getLog(Activator.PLUGIN_ID);
 					   project.hasNature(Constants.DS_VALIDATOR_PROJECT_NATURE) ||
 					   project.hasNature(Constants.ESB_PROJECT_NATURE) ||
 					   project.hasNature(Constants.JAXWS_PROJECT_NATURE) ||
+					   project.hasNature(Constants.JAXRS_PROJECT_NATURE) ||
 					   project.hasNature(Constants.WEBAPP_PROJECT_NATURE) ||
 					   project.hasNature(Constants.GADGET_PROJECT_NATURE) ||
 					   project.hasNature(Constants.LIBRARY_PROJECT_NATURE) ||
