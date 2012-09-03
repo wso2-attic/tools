0abafb809146716288269848b3cd034183d352c7
melan <melan@a5903396-d722-0410-b921-86c7d4935375>
2012-09-03 14:04:47 +0000

    removing 'reversed' property
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@140941 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.edit/src/org/wso2/developerstudio/eclipse/gmf/esb/provider/SequenceItemProvider.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.edit/src/org/wso2/developerstudio/eclipse/gmf/esb/provider/SequenceItemProvider.java
@@ -57,17 +57,18 @@ public class SequenceItemProvider
 	 * This returns the property descriptors for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	
 	@Override
 	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
-		if (itemPropertyDescriptors == null) {
-			super.getPropertyDescriptors(object);
-
-			addNamePropertyDescriptor(object);
-			addKeyPropertyDescriptor(object);
+		if (itemPropertyDescriptors != null) {
+			itemPropertyDescriptors.clear();
 		}
+		super.getPropertyDescriptors(object);
+
+		addNamePropertyDescriptor(object);
+		addKeyPropertyDescriptor(object);
 		return itemPropertyDescriptors;
 	}
 
