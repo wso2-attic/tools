e7c8aaa6d530a09b3c9fb69ff69b70cf1b977003
harshana <harshana@a5903396-d722-0410-b921-86c7d4935375>
2012-09-05 11:19:43 +0000

    Removing older versions of C-App module and project Eclipse plugins
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@141188 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/capp/org.wso2.developerstudio.eclipse.capp.project/src/org/wso2/developerstudio/eclipse/capp/project/preference/PreferenceConstants.java
+++
@@ -1,6 +0,0 @@
-package org.wso2.developerstudio.eclipse.capp.project.preference;
-
-public class PreferenceConstants {
-	public static final String CAPP_ARTIFACT_GROUP_SETTING = "group_capp_artifacts";
-
-}
