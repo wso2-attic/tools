5fef687a7cf1cbe6f0fd8ea60c539aa34600f01d
viraj <viraj@a5903396-d722-0410-b921-86c7d4935375>
2012-09-07 10:05:14 +0000

    Fixing a bug in IterateTargetOutputConnector
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@141593 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/edit/parts/IterateMediatorTargetOutputConnectorEditPart.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/edit/parts/IterateMediatorTargetOutputConnectorEditPart.java
@@ -133,7 +133,7 @@ public class IterateMediatorTargetOutputConnectorEditPart extends
 	protected NodeFigure createNodeFigure() {
 		NodeFigure figure = createNodePlate();
 		figure.setLayoutManager(new StackLayout());
-		IFigure shape = createNodeShape();
+		IFigure shape = createNodeShapeForward();
 		figure.add(shape);
 		contentPane = setupContentPane(shape);
 		figure_ = figure;
