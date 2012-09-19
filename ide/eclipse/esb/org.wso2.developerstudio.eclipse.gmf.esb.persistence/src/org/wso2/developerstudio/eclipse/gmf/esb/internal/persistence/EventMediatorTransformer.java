d3ad7df5984d35d5cfd1c96b1a2f5095a7c4d0cb
kalpa <kalpa@a5903396-d722-0410-b921-86c7d4935375>
2012-09-19 09:52:18 +0000

    Fixing namespace serialization issue.
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@143132 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.persistence/src/org/wso2/developerstudio/eclipse/gmf/esb/internal/persistence/EventMediatorTransformer.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.persistence/src/org/wso2/developerstudio/eclipse/gmf/esb/internal/persistence/EventMediatorTransformer.java
@@ -1,6 +1,8 @@
 package org.wso2.developerstudio.eclipse.gmf.esb.internal.persistence;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.synapse.endpoints.Endpoint;
 import org.apache.synapse.mediators.Value;
@@ -13,54 +15,90 @@ import org.wso2.developerstudio.eclipse.gmf.esb.EventMediator;
 import org.wso2.developerstudio.eclipse.gmf.esb.EventTopicType;
 import org.wso2.developerstudio.eclipse.gmf.esb.persistence.TransformationInfo;
 
-public class EventMediatorTransformer extends AbstractEsbNodeTransformer  {
+public class EventMediatorTransformer extends AbstractEsbNodeTransformer {
 
 	public void transform(TransformationInfo information, EsbNode subject)
 			throws Exception {
-		information.getParentSequence().addChild(createEventMediator(information,subject));
+		information.getParentSequence().addChild(
+				createEventMediator(information, subject));
 		// Transform the Event mediator output data flow path.
-		doTransform(information,
-				((EventMediator) subject).getOutputConnector());	
-		
+		doTransform(information, ((EventMediator) subject).getOutputConnector());
+
 	}
 
 	public void createSynapseObject(TransformationInfo info, EObject subject,
 			List<Endpoint> endPoints) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	public void transformWithinSequence(TransformationInfo information,
 			EsbNode subject, SequenceMediator sequence) throws Exception {
 		// TODO Auto-generated method stub
-		sequence.addChild(createEventMediator(information,subject));
-		doTransformWithinSequence(information,((EventMediator) subject).getOutputConnector().getOutgoingLink(),sequence);	
-		
+		sequence.addChild(createEventMediator(information, subject));
+		doTransformWithinSequence(information, ((EventMediator) subject)
+				.getOutputConnector().getOutgoingLink(), sequence);
+
 	}
-	
-	private org.wso2.carbon.mediator.event.EventMediator createEventMediator(TransformationInfo information,EsbNode subject) throws Exception{
+
+	private org.wso2.carbon.mediator.event.EventMediator createEventMediator(
+			TransformationInfo information, EsbNode subject) throws Exception {
 		// Check subject.
 		Assert.isTrue(subject instanceof EventMediator, "Invalid subject.");
 		EventMediator visualEvent = (EventMediator) subject;
-		
-		org.wso2.carbon.mediator.event.EventMediator eventMediator=new org.wso2.carbon.mediator.event.EventMediator();
+
+		org.wso2.carbon.mediator.event.EventMediator eventMediator = new org.wso2.carbon.mediator.event.EventMediator();
 		{
-			SynapseXPath expression=new SynapseXPath(visualEvent.getEventExpression().getPropertyValue());
-			eventMediator.setExpression(expression);
+			if (visualEvent.getEventExpression() != null
+					&& visualEvent.getEventExpression().getPropertyValue() != null) {
+				SynapseXPath expression = new SynapseXPath(visualEvent
+						.getEventExpression().getPropertyValue());
+
+				if (visualEvent.getEventExpression().getNamespaces() != null) {
+					Map<String, String> map = visualEvent.getEventExpression()
+							.getNamespaces();
+					Iterator<Map.Entry<String, String>> entries = map
+							.entrySet().iterator();
+					while (entries.hasNext()) {
+						Map.Entry<String, String> entry = entries.next();
+						expression.addNamespace(entry.getKey(),
+								entry.getValue());
+					}
+				}
+				eventMediator.setExpression(expression);
+			}
+
 			Value topic;
-			if(visualEvent.getTopicType().compareTo(EventTopicType.STATIC)==0){
-			topic = new Value(visualEvent.getStaticTopic());
+
+			if (visualEvent.getTopicType().compareTo(EventTopicType.STATIC) == 0) {
+
+				topic = new Value(visualEvent.getStaticTopic());
+
+			} else {
+
+				SynapseXPath topicExpression = new SynapseXPath(visualEvent
+						.getDynamicTopic().getPropertyValue());
+
+				if (visualEvent.getDynamicTopic().getNamespaces() != null) {
+					Map<String, String> map = visualEvent.getDynamicTopic()
+							.getNamespaces();
+					Iterator<Map.Entry<String, String>> entries = map
+							.entrySet().iterator();
+					while (entries.hasNext()) {
+						Map.Entry<String, String> entry = entries.next();
+						topicExpression.addNamespace(entry.getKey(),
+								entry.getValue());
+					}
+				}
+
+				topic = new Value(topicExpression);
+
 			}
-			else{
-			SynapseXPath topicExpression=new SynapseXPath(visualEvent.getDynamicTopic().getPropertyValue());	
-			topic = new Value(topicExpression);
-			}
+
 			eventMediator.setTopic(topic);
+
 		}
 		return eventMediator;
 	}
-
-
 
 }
