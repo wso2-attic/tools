af0ebd7fc4b32a35f1e94ba2da7cd32fa5a96f20
melan <melan@a5903396-d722-0410-b921-86c7d4935375>
2012-10-15 11:10:09 +0000

    Fixing TOOLS-1339
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@145655 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.artifact.proxyservice/src/org/wso2/developerstudio/eclipse/artifact/proxyservice/validators/EndPointsList.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.artifact.proxyservice/src/org/wso2/developerstudio/eclipse/artifact/proxyservice/validators/EndPointsList.java
@@ -1,3 +1,18 @@
+ /* Copyright (c) 2012, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.developerstudio.eclipse.artifact.proxyservice.validators;
 
 import java.io.File;
@@ -35,7 +50,13 @@ public class EndPointsList extends AbstractListDataProvider {
 			try{
 				if(project.isOpen()){
 					if(project.hasNature("org.wso2.developerstudio.eclipse.esb.project.nature")){
-						IResource resources[] = project.members();
+						IResource resources[] = project.getFolder("src/main/synapse-config/endpoints").members();
+						for (IResource res : resources) {
+							if(res.getName().endsWith(".xml")){
+								addToList(res,list);
+							}
+						}
+						resources = project.getFolder("src/main/synapse-config").members();
 						for (IResource res : resources) {
 							if(res.getName().endsWith(".xml")){
 								addToList(res,list);
