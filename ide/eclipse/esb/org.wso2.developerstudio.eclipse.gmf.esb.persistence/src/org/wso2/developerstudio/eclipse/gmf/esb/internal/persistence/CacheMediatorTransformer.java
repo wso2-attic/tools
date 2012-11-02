16edf5fb75e3be9df988da989020cf1f04b59af2
melan <melan@a5903396-d722-0410-b921-86c7d4935375>
2012-11-02 08:03:25 +0000

    Fixing cache-mediator serialization
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@147151 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.persistence/src/org/wso2/developerstudio/eclipse/gmf/esb/internal/persistence/CacheMediatorTransformer.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.persistence/src/org/wso2/developerstudio/eclipse/gmf/esb/internal/persistence/CacheMediatorTransformer.java
@@ -1,8 +1,26 @@
+/*
+ * Copyright 2012 WSO2, Inc. (http://wso2.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.developerstudio.eclipse.gmf.esb.internal.persistence;
 
 import java.util.List;
 
+import org.apache.synapse.config.xml.AnonymousListMediator;
 import org.apache.synapse.endpoints.Endpoint;
+import org.apache.synapse.mediators.ListMediator;
 import org.apache.synapse.mediators.base.SequenceMediator;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.emf.ecore.EObject;
@@ -12,11 +30,14 @@ import org.wso2.developerstudio.eclipse.gmf.esb.EsbNode;
 import org.wso2.developerstudio.eclipse.gmf.esb.RegistryKeyProperty;
 import org.wso2.developerstudio.eclipse.gmf.esb.persistence.TransformationInfo;
 
+/**
+ * Cache mediator transformer 
+ */
 public class CacheMediatorTransformer extends AbstractEsbNodeTransformer {
 
 	public void transform(TransformationInfo information, EsbNode subject)
 			throws Exception {
-		information.getParentSequence().addChild(createCacheMediator(subject));
+		information.getParentSequence().addChild(createCacheMediator(subject,information));
 		/**
 		 *  Transform the Cache Mediator output data flow path.
 		 */
@@ -31,12 +52,12 @@ public class CacheMediatorTransformer extends AbstractEsbNodeTransformer {
 
 	public void transformWithinSequence(TransformationInfo information,
 			EsbNode subject, SequenceMediator sequence) throws Exception {
-		sequence.addChild(createCacheMediator(subject));
+		sequence.addChild(createCacheMediator(subject,information));
 		doTransformWithinSequence(information,((CacheMediator) subject).getOutputConnector().getOutgoingLink(),sequence);
 		
 	}
 	
-	private org.apache.synapse.mediators.builtin.CacheMediator createCacheMediator(EsbNode subject) throws Exception{
+	private org.apache.synapse.mediators.builtin.CacheMediator createCacheMediator(EsbNode subject,TransformationInfo info) throws Exception{
 		/*
 		 *  Check subject.
 		 */
@@ -69,8 +90,23 @@ public class CacheMediatorTransformer extends AbstractEsbNodeTransformer {
 					 RegistryKeyProperty  regKeyProperty = visualCache.getSequenceKey();
 					 cacheMediator.setOnCacheHitRef(regKeyProperty.getKeyValue());
 				 }
-			 }
-			
+			} else {
+				
+				ListMediator onCacheHitMediatorList = new AnonymousListMediator();
+				SequenceMediator onCacheHitSequence = new SequenceMediator();
+
+				TransformationInfo newOnCacheHitInfo = new TransformationInfo();
+				newOnCacheHitInfo.setTraversalDirection(TransformationInfo.TRAVERSAL_DIRECTION_IN);
+				newOnCacheHitInfo.setSynapseConfiguration(info.getSynapseConfiguration());
+				newOnCacheHitInfo.setOriginInSequence(info.getOriginInSequence());
+				newOnCacheHitInfo.setOriginOutSequence(info.getOriginOutSequence());
+				newOnCacheHitInfo.setParentSequence(onCacheHitMediatorList);
+				doTransform(newOnCacheHitInfo, visualCache.getOnHitOutputConnector());
+
+				onCacheHitSequence.addAll(onCacheHitMediatorList.getList());
+				cacheMediator.setOnCacheHitSequence(onCacheHitSequence);
+			}
+
 		}
 		return cacheMediator;
 	}
