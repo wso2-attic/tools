021be3d8988e7e981ad2678e6d5feb1da8f59c6e
melan <melan@a5903396-d722-0410-b921-86c7d4935375>
2012-11-06 17:18:17 +0000

    Adding deserializer registry
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@147453 a5903396-d722-0410-b921-86c7d4935375

---
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.deserializer/src/org/wso2/developerstudio/eclipse/gmf/esb/deserializer/EsbDeserializerRegistry.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2012 WSO2, Inc. (http://wso2.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.developerstudio.eclipse.gmf.esb.deserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.synapse.mediators.builtin.LogMediator;
+import org.apache.synapse.mediators.builtin.PropertyMediator;
+import org.wso2.developerstudio.eclipse.gmf.esb.internal.deserializer.IEsbNodeDeserializer;
+import org.wso2.developerstudio.eclipse.gmf.esb.internal.deserializer.LogMediatorDeserializer;
+import org.wso2.developerstudio.eclipse.gmf.esb.internal.deserializer.PropertyMediatorDeserializer;
+
+
+/**
+ * A registry of synapse model to model object deserializers.
+ */
+public class EsbDeserializerRegistry {
+	/**
+	 * Singleton instance.
+	 */
+	private static EsbDeserializerRegistry singleton;		
+	
+	/**
+	 * synapse model type to deserializers map.
+	 */
+	private Map<Class<?>, IEsbNodeDeserializer> deserializersMap;
+	
+	/**
+	 * Creates a new deserialize registry.
+	 */
+	private EsbDeserializerRegistry(){
+		deserializersMap = new HashMap<Class<?>, IEsbNodeDeserializer>();
+		addDeserializer(LogMediator.class,new LogMediatorDeserializer());
+		addDeserializer(PropertyMediator.class,new PropertyMediatorDeserializer());
+	}
+	
+	/**
+	 * @return singleton instance.
+	 */
+	public static EsbDeserializerRegistry getInstance() {
+		if (null == singleton) {
+			singleton = new EsbDeserializerRegistry();
+		}
+		return singleton;
+	}
+	
+	/**
+	 * Adds a new deserializer into this registry.
+	 * 
+	 * @param synapseModelClass
+	 * @param transformer
+	 */
+	public void addDeserializer(Class<?> synapseModelClass, IEsbNodeDeserializer deserializer) {
+		deserializersMap.put(synapseModelClass, deserializer);
+	}
+	
+	/**
+	 * Attempts to locate a deserializer corresponding to the specified synapse model object.
+	 * @param synapseModel
+	 * @return
+	 */
+	public IEsbNodeDeserializer getDeserializer(Object synapseModel) {
+		return deserializersMap.get(synapseModel.getClass());
+	}
+
+}
