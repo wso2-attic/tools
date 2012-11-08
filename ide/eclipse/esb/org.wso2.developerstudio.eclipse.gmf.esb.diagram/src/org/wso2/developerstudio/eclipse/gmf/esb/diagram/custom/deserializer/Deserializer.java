f7dbb519b86cc2241e3b0677ab8aba6f3c26f454
melan <melan@a5903396-d722-0410-b921-86c7d4935375>
2012-11-08 14:22:05 +0000

    Adding support for synapse top-level elements in base deserializer
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@147648 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/custom/deserializer/Deserializer.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/custom/deserializer/Deserializer.java
@@ -19,9 +19,19 @@ package org.wso2.developerstudio.eclipse.gmf.esb.diagram.custom.deserializer;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+import org.apache.synapse.config.Entry;
 import org.apache.synapse.config.SynapseConfiguration;
 import org.apache.synapse.config.SynapseConfigurationBuilder;
+import org.apache.synapse.core.axis2.ProxyService;
+import org.apache.synapse.endpoints.Endpoint;
+import org.apache.synapse.endpoints.Template;
+import org.apache.synapse.mediators.base.SequenceMediator;
+import org.apache.synapse.mediators.template.TemplateMediator;
+import org.apache.synapse.rest.API;
 import org.eclipse.emf.common.command.CompoundCommand;
 import org.eclipse.emf.edit.command.AddCommand;
 import org.eclipse.emf.edit.command.RemoveCommand;
@@ -33,11 +43,21 @@ import org.wso2.developerstudio.eclipse.gmf.esb.EsbElement;
 import org.wso2.developerstudio.eclipse.gmf.esb.EsbNode;
 import org.wso2.developerstudio.eclipse.gmf.esb.EsbPackage;
 import org.wso2.developerstudio.eclipse.gmf.esb.EsbServer;
-import org.wso2.developerstudio.eclipse.gmf.esb.ProxyService;
 import org.wso2.developerstudio.eclipse.gmf.esb.diagram.Activator;
 import org.wso2.developerstudio.eclipse.gmf.esb.diagram.part.EsbDiagramEditor;
 import org.wso2.developerstudio.eclipse.logging.core.IDeveloperStudioLog;
 import org.wso2.developerstudio.eclipse.logging.core.Logger;
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.util.AXIOMUtil;
+import org.wso2.developerstudio.eclipse.gmf.esb.ArtifactType;
+import org.apache.synapse.config.xml.ProxyServiceFactory;
+import org.apache.synapse.config.xml.rest.APIFactory;
+import org.apache.synapse.config.xml.SequenceMediatorFactory;
+import org.apache.synapse.config.xml.EntryFactory;
+import org.apache.synapse.config.xml.endpoints.EndpointFactory;
+import org.apache.synapse.task.TaskDescription;
+import org.apache.synapse.task.TaskDescriptionFactory;
 
 /**
  * Synapse model de-serialize base class
@@ -80,54 +100,136 @@ public class Deserializer {
 		EsbDiagram esbDiagram = (EsbDiagram) diagram.getElement();
 		EsbServer esbServer = esbDiagram.getServer();
 		CompoundCommand resultCommand = new CompoundCommand();
-		File tempfile = File.createTempFile("file", ".tmp");
-		BufferedWriter outfile = new BufferedWriter(new FileWriter(tempfile));
-		outfile.write(source);
-		outfile.close();
-		SynapseConfiguration synapseCofig = SynapseConfigurationBuilder.getConfiguration(
-				tempfile.getAbsolutePath(), null);
+
 		TransactionalEditingDomain domain = TransactionUtil.getEditingDomain(esbServer);
 		// cleaning old diagram
 		// TODO: should be replaced by better approach
 		for (EsbElement child : esbServer.getChildren()) {
-			if (child instanceof ProxyService) {
-				RemoveCommand removeCmd = new RemoveCommand(domain, esbServer,
-						EsbPackage.Literals.ESB_SERVER__CHILDREN, child);
-				resultCommand.append(removeCmd);
-			}
+			RemoveCommand removeCmd = new RemoveCommand(domain, esbServer,
+					EsbPackage.Literals.ESB_SERVER__CHILDREN, child);
+			resultCommand.append(removeCmd);
 		}
-		
+
 		if (resultCommand.canExecute()) {
 			domain.getCommandStack().execute(resultCommand);
 		}
 
-		//resultCommand = new CompoundCommand();
 		AddCommand addCmd = null;
-		for (org.apache.synapse.core.axis2.ProxyService proxyService : synapseCofig
-				.getProxyServices()) {
-			
-			IEsbNodeDeserializer deserializer = EsbDeserializerRegistry.getInstance().getDeserializer(proxyService);
-			EsbNode node = deserializer.createNode(proxyService);
-			
-			if(deserializer!=null){
-				addCmd = new AddCommand(domain, esbServer, EsbPackage.Literals.ESB_SERVER__CHILDREN,
-						node);
-				if(addCmd.canExecute()){
+
+		Map<String, Object> artifacts = getArtifacts(source);
+		for (Map.Entry<String, Object> artifact : artifacts.entrySet()) {
+			IEsbNodeDeserializer deserializer = EsbDeserializerRegistry.getInstance()
+					.getDeserializer(artifact.getValue());
+			EsbNode node = deserializer.createNode(artifact.getValue());
+
+			if (deserializer != null) {
+				addCmd = new AddCommand(domain, esbServer,
+						EsbPackage.Literals.ESB_SERVER__CHILDREN, node);
+				if (addCmd.canExecute()) {
 					domain.getCommandStack().execute(addCmd);
-				} else{
-					log.warn("Cannot execute EMF command : "+ addCmd.toString());
+				} else {
+					log.warn("Cannot execute EMF command : " + addCmd.toString());
 				}
 			}
+		}
 
+		AbstractEsbNodeDeserializer.connectMediatorFlows();
+
+	}
+	
+	
+	private ArtifactType getArtifactType(String source) throws Exception{
+		ArtifactType artifactType = null;
+		OMElement element = AXIOMUtil.stringToOM(source);
+		String localName = element.getLocalName();
+		if("definitions".equals(localName)){
+			artifactType=ArtifactType.SYNAPSE_CONFIG;
+		} else if("proxy".equals(localName)){
+			artifactType=ArtifactType.PROXY;
+		} else if("sequence".equals(localName)){
+			artifactType=ArtifactType.SEQUENCE;
+		} else if("localEntry".equals(localName)){
+			artifactType=ArtifactType.LOCAL_ENTRY;
+		} else if("task".equals(localName)){
+			artifactType=ArtifactType.TASK;
+		} else if("api".equals(localName)){
+			artifactType=ArtifactType.API;
+		} else if("template".equals(localName)){
+			artifactType=ArtifactType.TEMPLATE;
+		} 
+		return artifactType;
+	}
+	
+	private Map<String,Object> getArtifacts(String source) throws Exception{
+		Map<String,Object> artifacts =new LinkedHashMap<String, Object>();
+		
+		ArtifactType artifactType = getArtifactType(source);
+		OMElement element = AXIOMUtil.stringToOM(source);
+		switch (artifactType) {
+		case SYNAPSE_CONFIG:
+			File tempfile = File.createTempFile("file", ".tmp");
+			BufferedWriter outfile = new BufferedWriter(new FileWriter(tempfile));
+			outfile.write(source);
+			outfile.close();
+			SynapseConfiguration synapseCofig = SynapseConfigurationBuilder.getConfiguration(
+					tempfile.getAbsolutePath(), null);
+			Collection<ProxyService> proxyServices = synapseCofig.getProxyServices();
+			for (ProxyService proxy : proxyServices) {
+				artifacts.put(proxy.getName(), proxy);
+			}
+			Collection<API> apis = synapseCofig.getAPIs();
+			for (API api : apis) {
+				artifacts.put(api.getName(), api);
+			}
+			Map<String, SequenceMediator> definedSequences = synapseCofig.getDefinedSequences();
+			if(definedSequences.size()>0)
+				artifacts.putAll(definedSequences);
+			Map<String, Endpoint> definedEndpoints = synapseCofig.getDefinedEndpoints();
+			if(definedEndpoints.size()>0)
+				artifacts.putAll(definedEndpoints);
+			Map<String, Entry> definedEntries = synapseCofig.getDefinedEntries();
+			if(definedEntries.size()>0)
+				artifacts.putAll(definedEntries);
+			Map<String, TemplateMediator> sequenceTemplates = synapseCofig.getSequenceTemplates();
+			if(sequenceTemplates.size()>0)
+				artifacts.putAll(sequenceTemplates);
+			Map<String, Template> endpointTemplates = synapseCofig.getEndpointTemplates();
+			if(endpointTemplates.size()>0)
+				artifacts.putAll(endpointTemplates);
+			break;
+		case PROXY:
+			ProxyService proxy = ProxyServiceFactory.createProxy(element, null);
+			artifacts.put(proxy.getName(), proxy);
+			break;
+		case SEQUENCE:
+			SequenceMediatorFactory sequenceMediatorFactory = new SequenceMediatorFactory();
+			sequenceMediatorFactory.createAnonymousSequence(element, null);
+			break;
+		case API:
+			API api = APIFactory.createAPI(element);
+			artifacts.put(api.getName(), api);
+			break;
+		case ENDPOINT:
+			Endpoint endpoint = EndpointFactory.getEndpointFromElement(element, false, null);
+			artifacts.put(endpoint.getName(), endpoint);
+			break;
+		case LOCAL_ENTRY:
+			Entry entry = EntryFactory.createEntry(element, null);
+			artifacts.put(entry.getKey(), entry);
+			break;
+		case TASK:
+			TaskDescription task = TaskDescriptionFactory.createTaskDescription(element, OMAbstractFactory.getOMFactory()
+					.createOMNamespace("http://ws.apache.org/ns/synapse", ""));
+			artifacts.put(task.getName(), task);
+			break;
+		case TEMPLATE:
+			//TODO : implement for templates
+			break;
+		default:
+			break;
 		}
 		
-		//TODO: deserialize other artifacts 
-		
-		AbstractEsbNodeDeserializer.connectMediatorFlows();
-				
+		return artifacts;
 	}
-
-
-	
 
 }
