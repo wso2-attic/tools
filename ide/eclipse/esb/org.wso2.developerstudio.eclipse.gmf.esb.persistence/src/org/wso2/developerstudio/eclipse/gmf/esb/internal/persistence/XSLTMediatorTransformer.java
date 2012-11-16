21db7d613837f39db7a0f6d148e44d7b0a16a287
melan <melan@a5903396-d722-0410-b921-86c7d4935375>
2012-11-16 05:20:57 +0000

    Fixing source-xpath and dynamic-schema-key serialization
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@148079 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.persistence/src/org/wso2/developerstudio/eclipse/gmf/esb/internal/persistence/XSLTMediatorTransformer.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.persistence/src/org/wso2/developerstudio/eclipse/gmf/esb/internal/persistence/XSLTMediatorTransformer.java
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2012 WSO2, Inc. (http://wso2.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.developerstudio.eclipse.gmf.esb.internal.persistence;
 
 import java.util.List;
+import java.util.Map.Entry;
 
-import org.apache.synapse.SynapseArtifact;
 import org.apache.synapse.endpoints.Endpoint;
 import org.apache.synapse.mediators.MediatorProperty;
 import org.apache.synapse.mediators.Value;
@@ -12,8 +28,8 @@ import org.apache.synapse.util.xpath.SynapseXPath;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.emf.ecore.EObject;
 import org.wso2.developerstudio.eclipse.gmf.esb.EsbNode;
+import org.wso2.developerstudio.eclipse.gmf.esb.KeyType;
 import org.wso2.developerstudio.eclipse.gmf.esb.NamespacedProperty;
-import org.wso2.developerstudio.eclipse.gmf.esb.PropertyMediator;
 import org.wso2.developerstudio.eclipse.gmf.esb.XSLTFeature;
 import org.wso2.developerstudio.eclipse.gmf.esb.XSLTMediator;
 import org.wso2.developerstudio.eclipse.gmf.esb.XSLTProperty;
@@ -50,11 +66,33 @@ public class XSLTMediatorTransformer extends AbstractEsbNodeTransformer {
 		
 		NamespacedProperty sourceXPath = visualXSLT.getSourceXPath();
 		if(sourceXPath.getPropertyValue()!=null && !sourceXPath.getPropertyValue().equals("")){
-			xsltMediator.setSource(new SynapseXPath(sourceXPath.getPropertyValue()));
+			SynapseXPath expression = new SynapseXPath(sourceXPath.getPropertyValue());
+			for (Entry<String, String> entry : sourceXPath.getNamespaces().entrySet()) {
+				expression.addNamespace(entry.getKey(), entry.getValue());
+			}
+			xsltMediator.setSource(expression);
 		}
-		String key = visualXSLT.getXsltStaticSchemaKey().getKeyValue();
-		if(key!=null && !key.equals("")){
-			xsltMediator.setXsltKey(new Value(key));
+		
+		Value xsltKey = null;
+		
+		if(visualXSLT.getXsltSchemaKeyType()==KeyType.DYNAMIC){
+			String key = visualXSLT.getXsltStaticSchemaKey().getKeyValue();
+			if(key!=null && !key.equals("")){
+				xsltKey = new Value(key);
+			}
+		} else{
+			NamespacedProperty key = visualXSLT.getXsltDynamicSchemaKey();
+			if(key.getPropertyValue()!=null && !key.getPropertyValue().equals("")){
+				SynapseXPath expression = new SynapseXPath(key.getPropertyValue());
+				for (Entry<String, String> entry : key.getNamespaces().entrySet()) {
+					expression.addNamespace(entry.getKey(), entry.getValue());
+				}
+				xsltKey = new Value(expression);
+			}
+		}
+		
+		if(xsltKey!=null){
+			xsltMediator.setXsltKey(xsltKey);
 		}
 		
 		for (XSLTFeature feature: visualXSLT.getFeatures()){
