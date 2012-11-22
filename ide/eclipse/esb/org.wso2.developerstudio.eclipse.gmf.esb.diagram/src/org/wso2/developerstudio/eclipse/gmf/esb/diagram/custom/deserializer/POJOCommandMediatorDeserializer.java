297f6b28e1fe0b5348dee80c381bf7b0bc4a0087
melan <melan@a5903396-d722-0410-b921-86c7d4935375>
2012-11-22 08:00:09 +0000

    Adding command-mediator properties deserializer
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@148945 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/custom/deserializer/POJOCommandMediatorDeserializer.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/custom/deserializer/POJOCommandMediatorDeserializer.java
@@ -16,13 +16,18 @@
 
 package org.wso2.developerstudio.eclipse.gmf.esb.diagram.custom.deserializer;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.synapse.mediators.AbstractMediator;
-import org.apache.synapse.util.xpath.SynapseXPath;
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
 import org.wso2.developerstudio.eclipse.gmf.esb.CommandMediator;
+import org.wso2.developerstudio.eclipse.gmf.esb.CommandProperty;
+import org.wso2.developerstudio.eclipse.gmf.esb.CommandPropertyContextAction;
+import org.wso2.developerstudio.eclipse.gmf.esb.CommandPropertyMessageAction;
+import org.wso2.developerstudio.eclipse.gmf.esb.CommandPropertyValueType;
+import org.wso2.developerstudio.eclipse.gmf.esb.EsbFactory;
 import org.wso2.developerstudio.eclipse.gmf.esb.diagram.providers.EsbElementTypes;
 import org.wso2.developerstudio.eclipse.gmf.esb.internal.persistence.custom.POJOCommandMediatorExt;
 import static org.wso2.developerstudio.eclipse.gmf.esb.EsbPackage.Literals.*;
@@ -40,15 +45,88 @@ public class POJOCommandMediatorDeserializer extends AbstractEsbNodeDeserializer
 		setElementToEdit(mediatorModel);
 		executeSetValueCommand(COMMAND_MEDIATOR__CLASS_NAME, mediator.getPojoClass());
 		
-		Map<String, String> contextGetterProperties = mediator.getContextGetterProperties();
-		Map<String, String> contextSetterProperties = mediator.getContextSetterProperties();
-		Map<String, SynapseXPath> messageGetterProperties = mediator.getMessageGetterProperties();
-		Map<String, SynapseXPath> messageSetterProperties = mediator.getMessageSetterProperties();
-		Map<String, Object> staticSetterProperties = mediator.getStaticSetterProperties();
+		Map<String,CommandProperty> properties = new HashMap<String,CommandProperty>();
 		
-		//TODO : Deserialize getter/setter properties
+		for (String propName : mediator.getStaticSetterProperties().keySet()) {
+			CommandProperty property = EsbFactory.eINSTANCE.createCommandProperty();
+			property.setPropertyName(propName);
+            Object value = mediator.getStaticSetterProperties().get(propName);
+            property.setValueLiteral(value.toString());
+            if (mediator.getContextGetterProperties().containsKey(propName)) {
+            	property.setValueContextPropertyName(mediator.getContextGetterProperties().get(propName).toString());
+            } else if (mediator.getMessageGetterProperties().containsKey(propName)) {
+            	property.setValueMessageElementXpath(createNamespacedProperty(mediator.getMessageGetterProperties().get(propName)));
+            }
+            property.setValueType(CommandPropertyValueType.LITERAL);
+            properties.put(propName, property);
+        }
+
+        for (String propName : mediator.getMessageSetterProperties().keySet()) {
+        	CommandProperty property = EsbFactory.eINSTANCE.createCommandProperty();
+			property.setPropertyName(propName);
+			property.setValueMessageElementXpath(createNamespacedProperty(mediator.getMessageGetterProperties().get(propName)));
+			property.setValueType(CommandPropertyValueType.MESSAGE_ELEMENT);
+            if (mediator.getMessageGetterProperties().containsKey(propName)) {
+            	property.setMessageAction(CommandPropertyMessageAction.READ_AND_UPDATE_MESSAGE);
+            } else if (mediator.getContextGetterProperties().containsKey(propName)) {
+            	property.setValueContextPropertyName(mediator.getContextGetterProperties().get(propName));
+            	property.setMessageAction(CommandPropertyMessageAction.READ_MESSAGE);
+            } else {
+            	property.setMessageAction(CommandPropertyMessageAction.READ_MESSAGE);                              
+            }
+            properties.put(propName, property);
+        }
+
+        for (String propName : mediator.getContextSetterProperties().keySet()) {
+        	CommandProperty property = EsbFactory.eINSTANCE.createCommandProperty();
+			property.setPropertyName(propName);
+			property.setValueContextPropertyName(mediator.getContextSetterProperties().get(propName));
+
+            if (mediator.getContextGetterProperties().containsKey(propName)) {
+            	property.setContextAction(CommandPropertyContextAction.READ_AND_UPDATE_CONTEXT);
+            } else if (mediator.getMessageGetterProperties().containsKey(propName)) {
+            	property.setContextAction(CommandPropertyContextAction.READ_CONTEXT);
+            	property.setValueMessageElementXpath(createNamespacedProperty(mediator.getMessageGetterProperties().get(propName)));
+            } else {
+            	property.setContextAction(CommandPropertyContextAction.READ_CONTEXT);               
+            }
+            property.setValueType(CommandPropertyValueType.CONTEXT_PROPERTY);
+            properties.put(propName, property);
+        }
+
+        for (String propName : mediator.getContextGetterProperties().keySet()) {
+            if (!isSerialized(propName, mediator)) {
+                String value = mediator.getContextGetterProperties().get(propName);
+            	CommandProperty property = EsbFactory.eINSTANCE.createCommandProperty();
+    			property.setPropertyName(propName);
+    			property.setValueContextPropertyName(value);
+                property.setContextAction(CommandPropertyContextAction.UPDATE_CONTEXT);
+                properties.put(propName, property);
+            }
+        }
+
+        for (String propName : mediator.getMessageGetterProperties().keySet()) {
+            if (!isSerialized(propName, mediator)) {
+            	CommandProperty property = EsbFactory.eINSTANCE.createCommandProperty();
+    			property.setPropertyName(propName);
+    			property.setMessageAction(CommandPropertyMessageAction.UPDATE_MESSAGE);
+    			property.setValueMessageElementXpath(createNamespacedProperty(mediator.getMessageGetterProperties().get(propName)));
+    			properties.put(propName, property);
+            }
+        }
+        
+        for (CommandProperty property : properties.values()) {
+			executeAddValueCommand(mediatorModel.getProperties(), property);
+		}
 		
 		return mediatorModel;
 	}
+	
+    private boolean isSerialized(String propName, POJOCommandMediatorExt m) {
+        return m.getContextSetterProperties().containsKey(propName) ||
+            m.getStaticSetterProperties().containsKey(propName) ||
+            m.getMessageSetterProperties().containsKey(propName);
+    }
+
 
 }
