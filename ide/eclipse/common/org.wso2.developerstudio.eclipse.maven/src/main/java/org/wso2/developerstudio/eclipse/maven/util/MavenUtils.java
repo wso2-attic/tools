d95372c9f27ddc4c0b8101dbb02366ef6f35d930
harshana <harshana@a5903396-d722-0410-b921-86c7d4935375>
2012-12-07 20:12:00 +0000

    Upgraded maven plugin versions
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@150731 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/common/org.wso2.developerstudio.eclipse.maven/src/main/java/org/wso2/developerstudio/eclipse/maven/util/MavenUtils.java
+++ ide/eclipse/common/org.wso2.developerstudio.eclipse.maven/src/main/java/org/wso2/developerstudio/eclipse/maven/util/MavenUtils.java
@@ -488,7 +488,7 @@ public class MavenUtils {
 		Plugin plugin;
 		
 		PluginExecution pluginExecution;
-		plugin = MavenUtils.createPluginEntry(mavenProject, "org.wso2.maven", "maven-bpel-plugin", "2.0.1", true);
+		plugin = MavenUtils.createPluginEntry(mavenProject, "org.wso2.maven", "maven-bpel-plugin", "2.0.2", true);
 //		pluginExecution=new PluginExecution();
 //		pluginExecution.addGoal("bpel");
 //		pluginExecution.setPhase("package");
