d84787cf1fa35564347d1d440f05dd79abb861c1
jasintha <jasintha@a5903396-d722-0410-b921-86c7d4935375>
2012-12-17 09:50:44 +0000

    updated the script mediator transformer
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@152001 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.persistence/src/org/wso2/developerstudio/eclipse/gmf/esb/internal/persistence/ScriptMediatorTransformer.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.persistence/src/org/wso2/developerstudio/eclipse/gmf/esb/internal/persistence/ScriptMediatorTransformer.java
@@ -1,13 +1,21 @@
 package org.wso2.developerstudio.eclipse.gmf.esb.internal.persistence;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.synapse.endpoints.Endpoint;
+import org.apache.synapse.mediators.Value;
 import org.apache.synapse.mediators.base.SequenceMediator;
 import org.eclipse.core.runtime.Assert;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.wso2.developerstudio.eclipse.gmf.esb.EsbNode;
+import org.wso2.developerstudio.eclipse.gmf.esb.RegistryKeyProperty;
 import org.wso2.developerstudio.eclipse.gmf.esb.ScriptMediator;
+import org.wso2.developerstudio.eclipse.gmf.esb.ScriptType;
+import org.wso2.developerstudio.eclipse.gmf.esb.scriptKeyTypeEnum;
 import org.wso2.developerstudio.eclipse.gmf.esb.persistence.TransformationInfo;
 
 public class ScriptMediatorTransformer extends AbstractEsbNodeTransformer {
@@ -33,16 +41,36 @@ public class ScriptMediatorTransformer extends AbstractEsbNodeTransformer {
 		// TODO Auto-generated method stub
 		sequence.addChild(createScriptMediator(subject));
 		doTransformWithinSequence(information,((ScriptMediator)subject).getOutputConnector().getOutgoingLink(),sequence);
-		
-		
 	}
 	
 	private org.apache.synapse.mediators.bsf.ScriptMediator createScriptMediator(EsbNode subject) throws Exception{
 		Assert.isTrue(subject instanceof ScriptMediator, "Invalid subject.");
 		ScriptMediator visualScript = (ScriptMediator)subject;
+		ScriptType scriptType = visualScript.getScriptType();
+		int scriptTypeValue = scriptType.getValue();
 		String language=visualScript.getScriptLanguage().getLiteral();
+		org.apache.synapse.mediators.bsf.ScriptMediator scriptMediator =null;
+		if(scriptTypeValue==0){
 		String scriptSourceCode=visualScript.getScriptBody();
-		org.apache.synapse.mediators.bsf.ScriptMediator scriptMediator=new org.apache.synapse.mediators.bsf.ScriptMediator(language,scriptSourceCode);
+	    scriptMediator = new org.apache.synapse.mediators.bsf.ScriptMediator(language,scriptSourceCode);
+		}else{
+			Map<Value, Object> includeMap = new HashMap<Value, Object>();
+			EList<RegistryKeyProperty> scriptKeys = visualScript.getScriptKeys();
+			for (RegistryKeyProperty registryKeyProperty : scriptKeys) {
+				Value value = new Value(registryKeyProperty.getKeyValue());
+				includeMap.put(value, null);
+			}
+			scriptKeyTypeEnum keyType = visualScript.getKeyType();
+			Value value=null;
+		    if(keyType.getValue()==keyType.STATIC_KEY_VALUE){	
+		    	value = new Value(visualScript.getScriptStaticKey().getKeyValue());
+		    }else{
+		    	
+		       value = new Value(visualScript.getScriptDynamicKey().getPropertyValue());
+		    }
+			scriptMediator = new org.apache.synapse.mediators.bsf.ScriptMediator(language,includeMap,value,visualScript.getMediateFunction());
+		}
+ 
 		return scriptMediator;
 	}
 
