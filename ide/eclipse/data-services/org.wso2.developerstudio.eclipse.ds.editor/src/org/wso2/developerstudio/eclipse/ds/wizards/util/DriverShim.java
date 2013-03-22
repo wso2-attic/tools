1bd86d194d61507fa29d4e2bc737595dcda6bceb
melan@wso2.com <melan@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-03-22 15:24:31 +0000

    Upgrading the trunk version from 3.0.0-SNAPSHOT to 3.1.0-SNAPSHOT
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@169004 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/data-services/org.wso2.developerstudio.eclipse.ds.editor/src/org/wso2/developerstudio/eclipse/ds/wizards/util/DriverShim.java
+++ ide/eclipse/data-services/org.wso2.developerstudio.eclipse.ds.editor/src/org/wso2/developerstudio/eclipse/ds/wizards/util/DriverShim.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverPropertyInfo;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 
 public class DriverShim implements Driver {
@@ -29,6 +31,11 @@ public class DriverShim implements Driver {
 	}
 	public boolean jdbcCompliant() {
 		return this.driver.jdbcCompliant();
+	}
+
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+	
+		return null;
 	}
 }
 
