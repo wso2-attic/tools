9dcf219a5e4796844917b92718a2bbf5d4dc7d73
viraj@wso2.com <viraj@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-04-22 19:20:02 +0000

    Fixing TOOLS-1627
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@170832 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.esb.project/src/org/wso2/developerstudio/eclipse/esb/project/provider/NavigatorActionProvider.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.esb.project/src/org/wso2/developerstudio/eclipse/esb/project/provider/NavigatorActionProvider.java
@@ -16,10 +16,15 @@
 
 package org.wso2.developerstudio.eclipse.esb.project.provider;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
@@ -40,6 +45,10 @@ import org.eclipse.ui.navigator.CommonActionProvider;
 import org.eclipse.ui.navigator.ICommonActionConstants;
 import org.eclipse.ui.navigator.ICommonActionExtensionSite;
 import org.eclipse.ui.part.FileEditorInput;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.wso2.developerstudio.eclipse.esb.project.Activator;
 import org.wso2.developerstudio.eclipse.general.project.artifact.GeneralProjectArtifact;
 import org.wso2.developerstudio.eclipse.general.project.artifact.RegistryArtifact;
@@ -47,14 +56,24 @@ import org.wso2.developerstudio.eclipse.general.project.artifact.bean.RegistryEl
 import org.wso2.developerstudio.eclipse.general.project.artifact.bean.RegistryItem;
 import org.wso2.developerstudio.eclipse.logging.core.IDeveloperStudioLog;
 import org.wso2.developerstudio.eclipse.logging.core.Logger;
+import org.wso2.developerstudio.eclipse.platform.ui.editor.Openable;
+import org.wso2.developerstudio.eclipse.platform.ui.startup.ESBGraphicalEditor;
+import org.wso2.developerstudio.eclipse.utils.file.FileUtils;
 
 /**
  * Custom NavigatorActionProvider for handling editor switching for ESB files
  * 
  */
 public class NavigatorActionProvider extends CommonActionProvider {
-	private static IDeveloperStudioLog log = Logger.getLog(Activator.PLUGIN_ID);
-
+	private static IDeveloperStudioLog log = Logger.getLog(Activator.PLUGIN_ID);	
+	private static final String SEQUENCE="sequence";
+	private static final String ENDPOINT="endpoint";
+	private static final String PROXY="proxy";
+	private static final String LOCAL_ENTRY="localentry";
+	private static final String TEMPLATE="template";
+	private static final String TASK="task";
+	private static final String API="api";
+	
 	private OpenEditorAction openEditorAction;
 	private static Map<String,String> prefixMap = new HashMap<String,String>();
 
@@ -132,15 +151,22 @@ public class NavigatorActionProvider extends CommonActionProvider {
 						fileTobeOpen = selection.getWorkspace().getRoot()
 						.getFile(path);
 							IDE.openEditor(page, fileTobeOpen);
-					} else {
-						fileTobeOpen = selection.getWorkspace().getRoot()
-								.getFile(new Path(synFilePath));
-						page.openEditor(new FileEditorInput(fileTobeOpen),
-								"org.wso2.developerstudio.eclipse.esb.presentation.EsbEditor");
+					} else {							
+						String[] type=getType(selection.getLocation().toOSString());
+						if("full-synapse".equals(type[0])){							
+							fileTobeOpen = selection.getWorkspace().getRoot().getFile(new Path(synFilePath));
+							page.openEditor(new FileEditorInput(fileTobeOpen),"org.wso2.developerstudio.eclipse.esb.presentation.EsbEditor");
+						}else{
+							String location=selection.getLocation().toOSString();				
+							String source = FileUtils.getContentAsString(new File(location));
+							String name =selection.getName().split("\\.")[0];													
+							String fullPath=selection.getFullPath().removeLastSegments(1).toOSString()+"/";
+							Openable openable = ESBGraphicalEditor.getOpenable();
+							openable.editorOpen(name,type[0],fullPath+type[1]+"_", source);
+						}
 					}
 				}
-
-			} catch (PartInitException e) {
+			}catch (Exception e) {
 				log.error("Can't open " + fileTobeOpen, e);
 			}
 		}
@@ -184,7 +210,108 @@ public class NavigatorActionProvider extends CommonActionProvider {
 			
 			return graphicalResource;
 		}
-
+		
+		/*
+		 * This method will return the type of the artifact(sequence, endpoint etc.) by reading the .xml file 
+		 */
+		private String[] getType(String path) throws Exception {
+			List<String> lines;
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+					.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			/*
+			 * Here we are using two approaches to get the correct synapse type
+			 * from the .xml file. 
+			 * 1. Use org.apache.commons.io.FileUtils.readLines(File) to read xml
+			 * basically and identify the correct type.
+			 * 2. If further investigation required other than step 1, use dom parser to parse
+			 * whole xml to get nested informations for evaluate for advanced
+			 * types such as main_sequence, template.endpoints etc.
+			 * 
+			 * 'type' is required in this stage due to architectural problem. So
+			 * we have to get-rid of following code after fixing the
+			 * architectural issues.
+			 */
+			lines = org.apache.commons.io.FileUtils.readLines(new File(path));
+			int lineCount = lines.size();
+			for (int i = 0; i < lineCount; ++i) {
+				String currentLine = lines.get(i);
+				if (currentLine.contains("<sequence")) {
+					Document doc = dBuilder.parse(new File(path));
+					NodeList nodes = doc.getElementsByTagName("sequence");
+					if (nodes.item(0).getNodeType() == Node.ELEMENT_NODE) {
+						Element eElement = (Element) nodes.item(0);
+						if ("main".equals(eElement.getAttribute("name"))) {
+							return new String[] { "main_sequence", SEQUENCE };
+						} else {
+							return new String[] { "sequence", SEQUENCE };
+						}
+					}
+				} else if (currentLine.contains("<proxy")) {
+					return new String[] { "proxy", PROXY };
+				} else if (currentLine.contains("<endpoint")) {
+					Document doc = dBuilder.parse(new File(path));
+					Node node = doc.getElementsByTagName("endpoint").item(0);
+					if (node.getNodeType() == Node.ELEMENT_NODE) {
+						Element eElement = (Element) node;
+						NodeList endpointChildNodes = eElement.getChildNodes();
+						for (int j = 0; j < endpointChildNodes.getLength(); ++j) {
+							if ("default".equals(endpointChildNodes.item(j)
+									.getNodeName())) {
+								return new String[] { "endpoint-0", ENDPOINT };
+							} else if ("address".equals(endpointChildNodes
+									.item(j).getNodeName())) {
+								return new String[] { "endpoint-1", ENDPOINT };
+							} else if ("wsdl".equals(endpointChildNodes.item(j)
+									.getNodeName())) {
+								return new String[] { "endpoint-2", ENDPOINT };
+							} else if ("loadbalance".equals(endpointChildNodes
+									.item(j).getNodeName())) {
+								return new String[] { "endpoint-3", ENDPOINT };
+							} else if ("failover".equals(endpointChildNodes
+									.item(j).getNodeName())) {
+								return new String[] { "endpoint-4", ENDPOINT };
+							} else if ("recipientlist"
+									.equals(endpointChildNodes.item(j)
+											.getNodeName())) {
+								return new String[] { "endpoint-5", ENDPOINT };
+							}
+						}
+					}
+				} else if (currentLine.contains("<localEntry")) {
+					return new String[] { "localentry", LOCAL_ENTRY };
+				} else if (currentLine.contains("<template")) {
+					Document doc = dBuilder.parse(new File(path));
+					Node template = doc.getElementsByTagName("template").item(0);
+					if (template.getNodeType() == Node.ELEMENT_NODE) {
+						Element eElement = (Element) template;
+						NodeList templateChildNodes = eElement.getChildNodes();
+						for (int j = 0; j < templateChildNodes.getLength(); ++j) {
+							if ("sequence".equals(templateChildNodes.item(j).getNodeName())) {
+								return new String[] { "template.sequence",TEMPLATE };
+							} else if ("endpoint".equals(templateChildNodes.item(j).getNodeName())) {
+								NodeList endpointChildNodes = templateChildNodes.item(j).getChildNodes();
+								for (int k = 0; k < endpointChildNodes.getLength(); ++k) {
+									if ("default".equals(endpointChildNodes.item(k).getNodeName())) {
+										return new String[] {"template.endpoint-0",TEMPLATE };
+									} else if ("address".equals(endpointChildNodes.item(k).getNodeName())) {
+										return new String[] {"template.endpoint-1",TEMPLATE };
+									} else if ("wsdl".equals(endpointChildNodes.item(k).getNodeName())) {
+										return new String[] {"template.endpoint-2",TEMPLATE };
+									}
+								}
+							}
+						}
+					}
+				} else if (currentLine.contains("<task")) {
+					return new String[] { "task", TASK };
+				} else if (currentLine.contains("<api")) {
+					return new String[] { "api", API };
+				} else if (currentLine.contains("<definitions")) {
+					return new String[] { "full-synapse", "full-synapse" };
+				}
+			}
+			return null;
+		}
 	}
-
 }
