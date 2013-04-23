715c9bd3253c4f3163138b04948e0e63da3e8b39
harshana@wso2.com <harshana@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-04-23 10:50:03 +0000

    Updated App factory login to use preference information
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@170904 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/appfactory/org.wso2.developerstudio.appfactory.ui/src/org/wso2/developerstudio/appfactory/ui/perspective/AppFactoryPerspectiveFactory.java
+++ ide/eclipse/appfactory/org.wso2.developerstudio.appfactory.ui/src/org/wso2/developerstudio/appfactory/ui/perspective/AppFactoryPerspectiveFactory.java
@@ -2,6 +2,7 @@ package org.wso2.developerstudio.appfactory.ui.perspective;
 
 import java.net.URL;
 
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.MessageBox;
@@ -16,6 +17,7 @@ import org.eclipse.ui.browser.IWebBrowser;
 import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
 import org.wso2.developerstudio.appfactory.ui.Activator;
 import org.wso2.developerstudio.appfactory.ui.connections.RestHttpsClient;
+import org.wso2.developerstudio.appfactory.ui.preference.AppFactoryPreferencePage;
 import org.wso2.developerstudio.appfactory.ui.views.PasswordDialog;
 
 public class AppFactoryPerspectiveFactory implements IPerspectiveFactory {
@@ -24,11 +26,20 @@ public class AppFactoryPerspectiveFactory implements IPerspectiveFactory {
 	private static final String APPDETILS_ID = "org.wso2.developerstudio.appfactory.ui.views.AppfactoryApplicationDetailsView";
 	private static final String APPBUILD_ID = "org.wso2.developerstudio.appfactory.ui.views.AppfactoryBuildInfoView";
     private static final String PROJECT_EXPOR_VIEW ="org.eclipse.ui.navigator.ProjectExplorer";
-    public static final String LOGIN_URL = "https://appfactorypreview.wso2.com/appmgt/site/blocks/user/login/ajax/login.jag";
+    
+    
+    public static final String DEFAULT_LOGIN_URL = "https://appfactorypreview.wso2.com/appmgt/site/blocks/user/login/ajax/login.jag";
 
 	private static IWebBrowser browser = null;
+	private IPreferenceStore preferenceStore;
 	
 	     public void createInitialLayout(IPageLayout myLayout) {
+	    	 preferenceStore = Activator.getDefault().getPreferenceStore();
+	    	 
+	    	 String appFactoryPreferenceURL = preferenceStore.getString(AppFactoryPreferencePage.APP_FACTORY_LOCATION);
+	    	 String appFactoryPreferenceUserName = preferenceStore.getString(AppFactoryPreferencePage.APP_FACTORY_USERNAME);
+	    	 String appFactoryPreferencePassword = preferenceStore.getString(AppFactoryPreferencePage.APP_FACTORY_PASSWORD);
+	    	 
 	    	 String editorArea = myLayout.getEditorArea();
 	    	 Shell activeShell = PlatformUI.getWorkbench().getDisplay().getActiveShell();
 	    	 IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
@@ -45,22 +56,22 @@ public class AppFactoryPerspectiveFactory implements IPerspectiveFactory {
 					if("true".equals("true")){
 						  IFolderLayout lef = myLayout.createFolder("topLeft",IPageLayout.LEFT,0.25f,editorArea);
 				   		  lef.addView(PROJECT_EXPOR_VIEW);
-				   	      IFolderLayout applist = myLayout.createFolder("topRight",IPageLayout.RIGHT,0.25f,editorArea);
+				   	      IFolderLayout applist = myLayout.createFolder("topRight",IPageLayout.RIGHT,0.75f,editorArea);
 				   	      applist.addView(APPLIST_ID);
-				   	      IFolderLayout appDetails = myLayout.createFolder("BottomRight",IPageLayout.BOTTOM,0.25f,"topRight");
+				   	      IFolderLayout appDetails = myLayout.createFolder("BottomRight",IPageLayout.BOTTOM,0.60f,"topRight");
 				   	      appDetails.addView(APPDETILS_ID);
-				   	      IFolderLayout buildInfo = myLayout.createFolder("Bottomt",IPageLayout.BOTTOM,0.25f,myLayout.getEditorArea());
+				   	      IFolderLayout buildInfo = myLayout.createFolder("Bottomt",IPageLayout.BOTTOM,0.60f,myLayout.getEditorArea());
 				   	      buildInfo.addView(APPBUILD_ID); 
 				   	      
 					// Opening the Web browser
-					IWorkbenchBrowserSupport browserSupport = Activator
-							.getDefault().getWorkbench().getBrowserSupport();
-
-					browser = browserSupport.createBrowser(
-							IWorkbenchBrowserSupport.LOCATION_BAR, null, null,
-							null);
+					IWorkbenchBrowserSupport browserSupport = Activator.getDefault().getWorkbench().getBrowserSupport();
+					browser = browserSupport.createBrowser(IWorkbenchBrowserSupport.LOCATION_BAR, null, null, null);
 					URL url = new URL("http://www.google.com/ncr");
-					browser.openURL(url);
+					if(appFactoryPreferenceURL!= null && !appFactoryPreferenceURL.equals("")){
+						browser.openURL(new URL(appFactoryPreferenceURL));
+					}else{
+						browser.openURL(url);
+					}
 				   	      
 					}else {
 						MessageBox messageBox = new MessageBox(activeShell,
