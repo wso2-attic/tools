db017c85a948faab1b82753b25c63ef96dd9971e
viraj@wso2.com <viraj@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-05-02 19:19:43 +0000

    Fixing TOOLS-1653
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@171479 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/custom/OpenSeparatelyEditPolicy.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/custom/OpenSeparatelyEditPolicy.java
@@ -4,6 +4,8 @@ import org.eclipse.gef.Request;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.OpenEditPolicy;
 import org.eclipse.gmf.runtime.notation.Node;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
 import org.wso2.developerstudio.eclipse.gmf.esb.NamedEndpoint;
 import org.wso2.developerstudio.eclipse.gmf.esb.Sequence;
 import org.wso2.developerstudio.eclipse.gmf.esb.diagram.edit.parts.NamedEndpointEditPart;
@@ -15,14 +17,30 @@ public class OpenSeparatelyEditPolicy extends OpenEditPolicy {
 		if (getTargetEditPart(request) instanceof SequenceEditPart) {
 			String name=((Sequence)((Node)((SequenceEditPart)getTargetEditPart(request)).getModel()).getElement()).getName();
 			if(!"{XPath}".equals(name) && ! (name.startsWith("conf:") || name.startsWith("gov:"))){
-				((SequenceEditPart) getTargetEditPart(request)).createDialogBox();
+				if(!name.contains(System.getProperty("file.separator"))){
+					((SequenceEditPart) getTargetEditPart(request)).createDialogBox();
+				}else {
+					MessageDialog
+					.openError(
+							Display.getCurrent().getActiveShell(),
+							"Invalid sequence name ! ",
+							"Cannot open the file with name \""+name+"\". Try removing file separator from the name if exists.");
+				}				
 			}
 			
 		}else if(getTargetEditPart(request) instanceof NamedEndpointEditPart){
 			String name=((NamedEndpoint)((Node)((NamedEndpointEditPart)getTargetEditPart(request)).getModel()).getElement()).getName();
 			if(name!=null){
-				if(!"{XPath}".equals(name) && !(name.startsWith("conf:") || name.startsWith("gov:"))){
-					((NamedEndpointEditPart) getTargetEditPart(request)).createDialogBox();
+				if(!"{XPath}".equals(name) && !(name.startsWith("conf:") || name.startsWith("gov:"))){					
+					if(!name.contains(System.getProperty("file.separator"))){
+						((NamedEndpointEditPart) getTargetEditPart(request)).createDialogBox();
+					}else {
+						MessageDialog
+						.openError(
+								Display.getCurrent().getActiveShell(),
+								"Invalid endpoint name ! ",
+								"Cannot open the file with name \""+name+"\". Try removing file separator from the name if exists.");
+					}					
 				}
 			}else{
 				((NamedEndpointEditPart) getTargetEditPart(request)).createDialogBox();
