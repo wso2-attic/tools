e9caa653305bfb43b1802762b6f1188febf6c986
susankha@wso2.com <susankha@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-08-08 05:59:09 +0000

    modify FunctionExecuter
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@180265 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/datamapper/org.wso2.datamapper/src/main/java/temp/FunctionExecuter.java
+++ ide/eclipse/datamapper/org.wso2.datamapper/src/main/java/temp/FunctionExecuter.java
@@ -2,32 +2,42 @@ package temp;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.antlr.v4.runtime.misc.NotNull;
+import org.apache.axiom.om.OMElement;
 import org.wso2.datamapper.parsers.MappingBaseListener;
 import org.wso2.datamapper.parsers.MappingParser.ArgContext;
 import org.wso2.datamapper.parsers.MappingParser.FunctionContext;
+import org.wso2.datamapper.parsers.MappingParser.FunctionnameContext;
+import org.wso2.datamapper.parsers.MappingParser.OutputelementContext;
 import org.wso2.datamapper.parsers.MappingParser.StatContext;
 import org.wso2.datamapper.core.InputDataHandler;
+import org.wso2.datamapper.dataModels.OutputDataModel;
 
 public class FunctionExecuter extends MappingBaseListener {
 
+	private String outputElement;
 	private String funcName;
-	private String result;
-	private List<String> argList;
-	InputDataHandler inputHandler;
+	private List<OMElement> argList;
+	private Map<String,String> resultMap;
+	private InputDataHandler inputHandler;
+	private String output = "";
+	private OutputDataModel outDataModel;
 
 	public FunctionExecuter() {
-		result = "";
-		argList = new ArrayList<String>();
-		inputHandler = new InputDataHandler();
+		outDataModel = new OutputDataModel();
+		argList = new ArrayList<OMElement>();
+		resultMap = new HashMap<String, String>();
+		inputHandler = new InputDataHandler("xml");
 		inputHandler.setInputFile(new File("./resource/input.xml"));
 	}
 
-	public String getResult() {
-		return result;
+	public Map<String, String> getResultMap() {
+		return resultMap;
 	}
 
 	@Override
@@ -35,31 +45,40 @@ public class FunctionExecuter extends MappingBaseListener {
 		super.exitArg(ctx);
 
 		String arg = ctx.getText();
-		String argValue = inputHandler.getInputvalue(arg);
-		this.argList.add(argValue);
-
-	}
-
-	@Override
-	public void exitStat(@NotNull StatContext ctx) {
-		if (this.funcName.equals("concat")) {
-			Iterator<String> it = this.argList.listIterator();
-
-			while (it.hasNext()) {
-				this.result = this.result.concat(" " + it.next());
-			}
-
-		} else if (this.funcName.equals("split")) {
-
-		} else {
-			System.out.println("not a method");
+		this.argList = inputHandler.getInputvalues(arg);	
+		Iterator<OMElement> it = this.argList.listIterator();
+		OMElement element;
+		
+		while (it.hasNext()) {
+			element = it.next();
+			if(this.funcName.equals("concat")){
+				output = output.concat(" "+element.getText());
+			}	
 		}
 	}
 
 	@Override
-	public void exitFunction(@NotNull FunctionContext ctx) {
-		super.exitFunction(ctx);
-		this.funcName = ctx.getText();
+	public void exitStat(@NotNull StatContext ctx) {
+		resultMap.put(outputElement, output);
 	}
 
+	@Override
+	public void exitFunction(@NotNull FunctionContext ctx) {		
+		System.out.println(this.outputElement+" == "+this.output);
+		resultMap.put(this.outputElement, this.output);
+	}
+
+	@Override
+	public void exitOutputelement(@NotNull OutputelementContext ctx) {
+		this.outputElement = ctx.getText();
+	}
+
+	@Override
+	public void exitFunctionname(@NotNull FunctionnameContext ctx) {
+		this.funcName = ctx.getText();
+		this.output = "";
+	}
+
+	
+	
 }
