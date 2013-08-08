61dddd9fa00108456bfeffaa8db037ed36c4adb3
susankha@wso2.com <susankha@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-08-08 06:03:35 +0000

    modify InputDataReaderAdapter
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@180268 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/datamapper/org.wso2.datamapper/src/main/java/org/wso2/datamapper/inputAdapters/InputDataReaderAdapter.java
+++ ide/eclipse/datamapper/org.wso2.datamapper/src/main/java/org/wso2/datamapper/inputAdapters/InputDataReaderAdapter.java
@@ -3,9 +3,11 @@ package org.wso2.datamapper.inputAdapters;
 import java.io.File;
 import java.util.List;
 
+import org.apache.axiom.om.OMElement;
+
 public interface InputDataReaderAdapter {
 
 	public void setInputReader(File inputFile);
-	public List<String> readInputvalues(String elementPath, int index);
+	public List<OMElement> readInputvalues(String elementPath);
 	
 }
