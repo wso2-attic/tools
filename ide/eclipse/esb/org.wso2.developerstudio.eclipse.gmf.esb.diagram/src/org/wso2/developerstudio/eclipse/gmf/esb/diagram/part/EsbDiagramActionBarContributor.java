33fe68cdcead6c6b8c3eb8518cd056a4eadba853
viraj@wso2.com <viraj@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-08-26 04:07:11 +0000

    Applying a patch from AsankaS for fixing TOOLS-1871
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@183030 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/part/EsbDiagramActionBarContributor.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/part/EsbDiagramActionBarContributor.java
@@ -1,7 +1,9 @@
 package org.wso2.developerstudio.eclipse.gmf.esb.diagram.part;
 
+import org.eclipse.gmf.runtime.diagram.ui.actions.ActionIds;
 import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramActionBarContributor;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchPage;
@@ -35,5 +37,15 @@ public class EsbDiagramActionBarContributor extends DiagramActionBarContributor 
 				IWorkbenchActionConstants.M_FILE);
 		assert fileMenu != null;
 		fileMenu.remove("pageSetupAction"); //$NON-NLS-1$
+		
+		// Removing Diagram menu from ESB editor.
+		IMenuManager diagramMenu = bars.getMenuManager().findMenuUsingPath(ActionIds.MENU_DIAGRAM);
+		assert diagramMenu != null;
+		bars.getMenuManager().remove("diagramMenu");
+
+		// Remove Diagram toolbar from ESB editor..
+		IToolBarManager toolBarManager = bars.getToolBarManager();
+		assert toolBarManager != null;
+		toolBarManager.removeAll();
 	}
 }
