7f61d7a6bdce7433e0596e781c37ee52f25f891d
susankha@wso2.com <susankha@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-09-12 04:15:33 +0000

    modify input readers
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@185192 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/datamapper/org.wso2.datamapper/src/main/java/org/wso2/datamapper/inputAdapters/XmlInputReader.java
+++ ide/eclipse/datamapper/org.wso2.datamapper/src/main/java/org/wso2/datamapper/inputAdapters/XmlInputReader.java
@@ -20,7 +20,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
@@ -28,12 +30,14 @@ import org.apache.avro.generic.GenericRecord;
 import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.OMXMLBuilderFactory;
 import org.apache.axiom.om.OMXMLParserWrapper;
+import org.apache.axiom.om.xpath.AXIOMXPath;
+import org.jaxen.JaxenException;
 
 public class XmlInputReader implements InputDataReaderAdapter {
 
 	private OMElement documentElement;
-	private GenericRecord inputData;
-
+	private List<GenericRecord> inputRecords;	
+	
 	@Override
 	public void setInputReader(File inputFile) {
 		try {
@@ -42,22 +46,41 @@ public class XmlInputReader implements InputDataReaderAdapter {
 			this.documentElement = builder.getDocumentElement();
 
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
 	@Override
-	public GenericRecord readInputvalues(Schema schema) {
-		
-		Iterator<OMElement> childElements = this.documentElement.getChildElements();
-		inputData = new GenericData.Record(schema);
-		
-		while (childElements.hasNext()) {
-			OMElement child = childElements.next();
-			inputData.put(child.getLocalName(),child.getText());
-		}
-		return this.inputData;
+	public OMElement getRootElement() {
+		return this.documentElement;
 	}
 
+	@Override
+	public Iterator<GenericRecord> getChildItr(String xpath, Schema schema) {
+		this.inputRecords = new ArrayList<GenericRecord>();
+		GenericRecord inputRecord;
+		try {
+			AXIOMXPath xpathExpression = new AXIOMXPath (xpath);
+			Iterator<OMElement> machinElementsItr = xpathExpression.selectNodes(this.documentElement).listIterator();
+			
+			while (machinElementsItr.hasNext()) {
+				OMElement element = machinElementsItr.next();				
+				
+				Iterator<OMElement> childs = element.getChildElements();
+				inputRecord = new GenericData.Record(schema);	
+				
+				while (childs.hasNext()) {
+					OMElement childElement = childs.next();
+					inputRecord.put(childElement.getLocalName(), childElement.getText());
+				}
+				this.inputRecords.add(inputRecord);
+			}
+			
+		} catch (JaxenException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 	
+		return this.inputRecords.listIterator();
+	}
+	
 }
