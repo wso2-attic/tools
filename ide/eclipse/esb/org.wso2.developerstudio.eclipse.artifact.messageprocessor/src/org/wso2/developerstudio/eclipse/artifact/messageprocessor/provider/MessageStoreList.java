9a2715db1a68adf8af2df07e52db95b3d61cd11b
susankha@wso2.com <susankha@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-09-26 09:03:25 +0000

    Applying a patch from Sohani for fixing TOOLS-1968
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@186492 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.artifact.messageprocessor/src/org/wso2/developerstudio/eclipse/artifact/messageprocessor/provider/MessageStoreList.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.artifact.messageprocessor/src/org/wso2/developerstudio/eclipse/artifact/messageprocessor/provider/MessageStoreList.java
@@ -19,13 +19,12 @@ package org.wso2.developerstudio.eclipse.artifact.messageprocessor.provider;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.xml.stream.FactoryConfigurationError;
-
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.wso2.developerstudio.eclipse.artifact.messageprocessor.Activator;
+import org.wso2.developerstudio.eclipse.artifact.messageprocessor.model.MessageProcessorModel;
 import org.wso2.developerstudio.eclipse.esb.project.artifact.ESBArtifact;
 import org.wso2.developerstudio.eclipse.esb.project.artifact.ESBProjectArtifact;
 import org.wso2.developerstudio.eclipse.logging.core.IDeveloperStudioLog;
@@ -36,30 +35,32 @@ import org.wso2.developerstudio.eclipse.platform.core.project.model.ProjectDataM
 import org.wso2.developerstudio.eclipse.utils.project.ProjectUtils;
 
 public class MessageStoreList extends AbstractListDataProvider{
-	
 	private static IDeveloperStudioLog log=Logger.getLog(Activator.PLUGIN_ID);
 
 	@Override
 	public List<ListData> getListData(String modelProperty,ProjectDataModel model) {
 		
-		List<ListData> availableStoreList = new ArrayList<ListData>();
+		List<ListData> availableMessageStoreList = new ArrayList<ListData>();
+		
+		MessageProcessorModel messageProcessorModel =(MessageProcessorModel) model;
+		
+		 
+		IProject projects = messageProcessorModel.getSaveLocation().getProject();
 		
 		
-		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
-		for (IProject workspaceProject : projects) {
-			if (workspaceProject.isOpen()) {
+		
 				try {
-					if (workspaceProject.hasNature("org.wso2.developerstudio.eclipse.esb.project.nature")) {
+					if (projects.hasNature("org.wso2.developerstudio.eclipse.esb.project.nature")) {
 						
 						ESBProjectArtifact esbProjectArtifact = new ESBProjectArtifact();
 						try {
-							esbProjectArtifact.fromFile(workspaceProject.getFile("artifact.xml").getLocation().toFile());
+							esbProjectArtifact.fromFile(projects.getFile("artifact.xml").getLocation().toFile());
 							List<ESBArtifact> allESBArtifacts = esbProjectArtifact.getAllESBArtifacts();
 							for (ESBArtifact esbArtifact : allESBArtifacts) {
 								if(esbArtifact.getType().equals("synapse/message-store")){
 									String relativeFilePath = esbArtifact.getFile().replaceAll("/", (File.separatorChar=='/')?"/":"\\\\");
 									String fileName = new File((relativeFilePath)).getName().replaceAll(".xml$", "");
-									availableStoreList.add(createListData(fileName,fileName));
+									availableMessageStoreList.add(createListData(fileName,fileName));
 								}
 							}
 						} catch (Exception e) {
@@ -69,10 +70,9 @@ public class MessageStoreList extends AbstractListDataProvider{
 				} catch (Exception e) {
 					log.error("Error occured while scanning the workspace for Message-Store artifacts", e);
 				} 
-			}
-		}
 		
-		return availableStoreList;
+		
+		return availableMessageStoreList;
 	}
 
 }
