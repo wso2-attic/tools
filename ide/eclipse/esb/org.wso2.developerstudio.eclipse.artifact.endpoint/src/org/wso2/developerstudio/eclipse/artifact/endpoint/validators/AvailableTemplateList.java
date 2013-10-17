601f8b5e8a45dd6b29ebb7f5a3234721a8a973e9
viraj@wso2.com <viraj@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-10-17 09:55:51 +0000

    Applying a patch from Sohani for fixing TOOLS-2037
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@188578 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.artifact.endpoint/src/org/wso2/developerstudio/eclipse/artifact/endpoint/validators/AvailableTemplateList.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.artifact.endpoint/src/org/wso2/developerstudio/eclipse/artifact/endpoint/validators/AvailableTemplateList.java
@@ -3,49 +3,18 @@ package org.wso2.developerstudio.eclipse.artifact.endpoint.validators;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.xml.stream.FactoryConfigurationError;
-
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.ui.*;
 import org.eclipse.ui.internal.WorkbenchWindow;
-import org.eclipse.ui.part.FileEditorInput;
 import org.wso2.developerstudio.eclipse.artifact.endpoint.Activator;
-import org.wso2.developerstudio.eclipse.artifact.endpoint.model.EndpointModel;
 import org.wso2.developerstudio.eclipse.esb.project.artifact.ESBArtifact;
 import org.wso2.developerstudio.eclipse.esb.project.artifact.ESBProjectArtifact;
 import org.wso2.developerstudio.eclipse.logging.core.IDeveloperStudioLog;
 import org.wso2.developerstudio.eclipse.logging.core.Logger;
 import org.wso2.developerstudio.eclipse.platform.core.model.AbstractListDataProvider;
-import org.wso2.developerstudio.eclipse.platform.core.model.AbstractListDataProvider.ListData;
 import org.wso2.developerstudio.eclipse.platform.core.project.model.ProjectDataModel;
-import org.wso2.developerstudio.eclipse.utils.project.ProjectUtils;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.jface.text.ITextViewer;
-import org.eclipse.jface.text.source.CompositeRuler;
-import org.eclipse.jface.text.source.LineNumberRulerColumn;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.part.FileEditorInput;
-import org.eclipse.ui.views.navigator.ResourceNavigator;
-import org.osgi.framework.Bundle;
-import org.eclipse.ui.IEditorPart;
+
 
 public class AvailableTemplateList extends AbstractListDataProvider{
 
@@ -55,19 +24,18 @@ public class AvailableTemplateList extends AbstractListDataProvider{
 	public List<ListData> getListData(String modelProperty,ProjectDataModel model) {
 		
 		List<ListData> availableStoreList = new ArrayList<ListData>();
-		
-		EndpointModel endpointModel = (EndpointModel) model;
-		 
-		IProject projects = endpointModel.getEndpointSaveLocation().getProject();
+		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
 		
 		
+		
+		for (IProject workspaceProject : projects) {
 		
 				try {
-					if (projects.hasNature("org.wso2.developerstudio.eclipse.esb.project.nature")) {
+					if (workspaceProject.isOpen() && workspaceProject.hasNature("org.wso2.developerstudio.eclipse.esb.project.nature")) {
 						
 						ESBProjectArtifact esbProjectArtifact = new ESBProjectArtifact();
 						try {
-							esbProjectArtifact.fromFile(projects.getFile("artifact.xml").getLocation().toFile());
+							esbProjectArtifact.fromFile(workspaceProject.getFile("artifact.xml").getLocation().toFile());
 							List<ESBArtifact> allESBArtifacts = esbProjectArtifact.getAllESBArtifacts();
 							for (ESBArtifact esbArtifact : allESBArtifacts) {
 								if(esbArtifact.getType().equals("synapse/endpointTemplate")){
@@ -80,12 +48,16 @@ public class AvailableTemplateList extends AbstractListDataProvider{
 							log.error("Error occured while scanning the workspace for Message-Store artifacts", e);
 						}
 					}
+					else {
+						
+					}
 				} catch (Exception e) {
 					log.error("Error occured while scanning the workspace for Message-Store artifacts", e);
 				} 
-		
+		}
+	
 		
 		return availableStoreList;
 	}
-
 }
+
--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.artifact.messageprocessor/src/org/wso2/developerstudio/eclipse/artifact/messageprocessor/provider/MessageStoreList.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.artifact.messageprocessor/src/org/wso2/developerstudio/eclipse/artifact/messageprocessor/provider/MessageStoreList.java
@@ -19,20 +19,16 @@ package org.wso2.developerstudio.eclipse.artifact.messageprocessor.provider;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import javax.xml.stream.FactoryConfigurationError;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 import org.wso2.developerstudio.eclipse.artifact.messageprocessor.Activator;
-import org.wso2.developerstudio.eclipse.artifact.messageprocessor.model.MessageProcessorModel;
 import org.wso2.developerstudio.eclipse.esb.project.artifact.ESBArtifact;
 import org.wso2.developerstudio.eclipse.esb.project.artifact.ESBProjectArtifact;
 import org.wso2.developerstudio.eclipse.logging.core.IDeveloperStudioLog;
 import org.wso2.developerstudio.eclipse.logging.core.Logger;
 import org.wso2.developerstudio.eclipse.platform.core.model.AbstractListDataProvider;
-import org.wso2.developerstudio.eclipse.platform.core.model.AbstractListDataProvider.ListData;
 import org.wso2.developerstudio.eclipse.platform.core.project.model.ProjectDataModel;
-import org.wso2.developerstudio.eclipse.utils.project.ProjectUtils;
+
 
 public class MessageStoreList extends AbstractListDataProvider{
 	private static IDeveloperStudioLog log=Logger.getLog(Activator.PLUGIN_ID);
@@ -42,37 +38,38 @@ public class MessageStoreList extends AbstractListDataProvider{
 		
 		List<ListData> availableMessageStoreList = new ArrayList<ListData>();
 		
-		MessageProcessorModel messageProcessorModel =(MessageProcessorModel) model;
-		
-		 
-		IProject projects = messageProcessorModel.getSaveLocation().getProject();
-		
-		
-		
+		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+		for (IProject workspaceProject : projects) {
+			
 				try {
-					if (projects.hasNature("org.wso2.developerstudio.eclipse.esb.project.nature")) {
-						
-						ESBProjectArtifact esbProjectArtifact = new ESBProjectArtifact();
-						try {
-							esbProjectArtifact.fromFile(projects.getFile("artifact.xml").getLocation().toFile());
-							List<ESBArtifact> allESBArtifacts = esbProjectArtifact.getAllESBArtifacts();
-							for (ESBArtifact esbArtifact : allESBArtifacts) {
-								if(esbArtifact.getType().equals("synapse/message-store")){
-									String relativeFilePath = esbArtifact.getFile().replaceAll("/", (File.separatorChar=='/')?"/":"\\\\");
-									String fileName = new File((relativeFilePath)).getName().replaceAll(".xml$", "");
+					if (workspaceProject.isOpen() && workspaceProject.hasNature("org.wso2.developerstudio.eclipse.esb.project.nature")) {
+					
+ 						
+ 						ESBProjectArtifact esbProjectArtifact = new ESBProjectArtifact();
+ 						try {
+							esbProjectArtifact.fromFile(workspaceProject.getFile("artifact.xml").getLocation().toFile());
+							
+ 							List<ESBArtifact> allESBArtifacts = esbProjectArtifact.getAllESBArtifacts();
+ 							for (ESBArtifact esbArtifact : allESBArtifacts) {
+ 								if(esbArtifact.getType().equals("synapse/message-store")){
+ 									String relativeFilePath = esbArtifact.getFile().replaceAll("/", (File.separatorChar=='/')?"/":"\\\\");
+ 									String fileName = new File((relativeFilePath)).getName().replaceAll(".xml$", "");
 									availableMessageStoreList.add(createListData(fileName,fileName));
-								}
-							}
-						} catch (Exception e) {
-							log.error("Error occured while scanning the workspace for Message-Store artifacts", e);
-						}
-					}
-				} catch (Exception e) {
-					log.error("Error occured while scanning the workspace for Message-Store artifacts", e);
-				} 
-		
-		
-		return availableMessageStoreList;
-	}
+ 								}
+ 							}
+ 						}
+ 						
+ 						catch (Exception e) {
 
-}
+ 		 				} 
+					}
+				}
+					catch (Exception e) {
+ 		 					log.error("Error occured while scanning the workspace for Message-Store artifacts", e);
+ 		 				} 
+ 					}
+ 		 		
+ 				return availableMessageStoreList;
+ 		 	}
+ 		 
+ 		 }
