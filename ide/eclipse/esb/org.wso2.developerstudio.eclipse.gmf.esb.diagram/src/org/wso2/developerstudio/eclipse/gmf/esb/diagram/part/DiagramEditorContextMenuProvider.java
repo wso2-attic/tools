21c25985957e7b807efe7953dd365f4f471fb5fd
viraj@wso2.com <viraj@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2014-01-15 13:17:47 +0000

    Applying a patch from GayanK for reverting the fix for TOOLS-2207
    git-svn-id: https://svn.wso2.org/repos/wso2/trunk/tools@194540 a5903396-d722-0410-b921-86c7d4935375

--- ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/part/DiagramEditorContextMenuProvider.java
+++ ide/eclipse/esb/org.wso2.developerstudio.eclipse.gmf.esb.diagram/src/org/wso2/developerstudio/eclipse/gmf/esb/diagram/part/DiagramEditorContextMenuProvider.java
@@ -117,9 +117,9 @@ public class DiagramEditorContextMenuProvider extends DiagramContextMenuProvider
 	private IWorkbenchPart part;
 
 	/**
-	 * @generated NOT
+	 * @generated
 	 */
-	//private DeleteElementAction deleteAction;
+	private DeleteElementAction deleteAction;
 
 	/**
 	 * Model export action.
@@ -142,8 +142,8 @@ public class DiagramEditorContextMenuProvider extends DiagramContextMenuProvider
 		super(part, viewer);
 		this.part = part;
 
-		/*deleteAction = new DeleteElementAction(part);
-		deleteAction.init();*/
+		deleteAction = new DeleteElementAction(part);
+		deleteAction.init();
 		exportAction = new EsbModelExportAction(part);
 		exportAction.init();
 
@@ -274,11 +274,10 @@ public class DiagramEditorContextMenuProvider extends DiagramContextMenuProvider
 	 * @generated NOT
 	 */
 	public void dispose() {
-		// Fixing TOOLS-2207
-		/*if (deleteAction != null) {
+		if (deleteAction != null) {
 			deleteAction.dispose();
 			deleteAction = null;
-		}*/
+		}
 
 		if (null != exportAction) {
 			exportAction.dispose();
@@ -317,8 +316,7 @@ public class DiagramEditorContextMenuProvider extends DiagramContextMenuProvider
 							// Remove Format menu item. 
 							menu.remove(ActionIds.MENU_FORMAT);
 
-							// Fixing TOOLS-2207
-							//menu.appendToGroup("editGroup", deleteAction);
+							menu.appendToGroup("editGroup", deleteAction);
 							menu.remove(EsbModelExportAction.ACTION_ID);
 							//menu.appendToGroup("editGroup", exportAction);
 
